4afddc25aa7894328446bc7be82c058cc8bda41e
Simone <smarras@njit.edu>
2022-02-12 14:14:08 -0500

    wip: src/basis/basis_structs.jl, src/basis/build_lgl.jl

--- runs/testgmsh.jl
+++ runs/testgmsh.jl
@@ -5,19 +5,7 @@ using MPI
 
 model = GmshDiscreteModel("/Users/simone/Work/Codes/jexpresso/demo/gmsh_grids/hexa_UNSTR.msh")
 
+#model.grid.node_coordinates <-- coords
+#model.grid.cell_node_ids    <-- conn
+
 writevtk(model,"gmsh_grid")
-
-
-order = 10
-reffe = ReferenceFE(lagrangian,Float64,order)
-#=
-V = TestFESpace(model,reffe,dirichlet_tags=["boundary1","boundary2"])
-U = TrialFESpace(V,[0,1])
-Ω = Triangulation(model)
-dΩ = Measure(Ω,2*order)
-a(u,v) = ∫( ∇(v)⋅∇(u) )dΩ
-l(v) = 0
-op = AffineFEOperator(a,l,U,V)
-uh = solve(op)
-writevtk(Ω,"gmsh_grid"); #,cellfields=["uh"=>uh])
-=#
