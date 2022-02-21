9286e2d6c8640bf57eaa160b5a7d7ee7cbd8ffd5
Simone <smarras@njit.edu>
2022-02-21 12:24:35 -0500

    WIP Added ODE solver functions in solver/mod_solution.jl

--- src/main.jl
+++ src/main.jl
@@ -9,7 +9,7 @@ using MPI
 using Revise
 
 #Plots
-using Plots
+using Plots; gr()
 plotlyjs()
 
 #Constants
@@ -28,79 +28,110 @@ include("./solver/mod_solution.jl")
 #comm = MPI.COMM_WORLD
 
 #if MPI.Comm_rank(comm) == 0
-    print(BLUE_FG(" #--------------------------------------------------------------------------------\n"))
-    print(BLUE_FG(" # Welcome to ", RED_FG("jexpresso\n")))
-    print(BLUE_FG(" # A Julia code to solve turbulence problems in the atmosphere\n"))
+print(BLUE_FG(" #--------------------------------------------------------------------------------\n"))
+print(BLUE_FG(" # Welcome to ", RED_FG("jexpresso\n")))
+print(BLUE_FG(" # A Julia code to solve turbulence problems in the atmosphere\n"))
 
 
-    #--------------------------------------------------------
-    #User inputs:
-    #--------------------------------------------------------
-    inputs, nvars = mod_inputs_user_inputs(TInt, TFloat)
-    
-    #--------------------------------------------------------
-    #Print inputs to screen
-    #--------------------------------------------------------
-    #=
-    NOTE: SM
-    ADD HERE A FUNCTION TO CHECK IF SOME NECESSARY INPUTS WERE NOT DEFINED
-    THINK OF A WAY TO CREATE A DYNAMIC INPUT SETUP to make the inputs list flexible.
-    =#
-    println( " #--------------------------------------------------------------------------------")
-    print(GREEN_FG(" # User inputs:\n"))
-    println( " # Equation set:  ", inputs[:equation_set])
-    println( " # Problem:       ", inputs[:problem])
-    println( " # N. variables:  ", nvars)
-    println( " # N. space dims: ", inputs[:nsd])
-    println( " # N. x-points:   ", inputs[:npx])
-    println( " # [xmin, xmax]:  ", inputs[:xmin], " ", inputs[:xmax])
-    if (inputs[:nsd] > 1)
-        println( " # N. y-points:   ", inputs[:npy])
-        println( " # [ymin, ymax]:  ", inputs[:ymin], " ", inputs[:ymax])
-    end
-    if (inputs[:nsd] == 3)
-        println( " # N. z-points:   ", inputs[:npz])
-        println( " # [zmin, zmax]:  ", inputs[:zmin], " ", inputs[:zmax])
-    end
+#--------------------------------------------------------
+#User inputs:
+#--------------------------------------------------------
+inputs, nvars = mod_inputs_user_inputs(TInt, TFloat)
 
-    #--------------------------------------------------------
-    # Build mesh    
-    #--------------------------------------------------------
-    #
-    # Initialize mesh struct
-    mesh = St_mesh{TInt,TFloat}(zeros(inputs[:npx]), zeros(inputs[:npy]), zeros(inputs[:npz]),
-                                 inputs[:xmin], inputs[:xmax],
-                                 inputs[:ymin], inputs[:ymax],
-                                 inputs[:zmin], inputs[:zmax],
-                                 inputs[:npx], inputs[:npy], inputs[:npz])
+#--------------------------------------------------------
+#Print inputs to screen
+#--------------------------------------------------------
+#=
+NOTE: SM
+ADD HERE A FUNCTION TO CHECK IF SOME NECESSARY INPUTS WERE NOT DEFINED
+THINK OF A WAY TO CREATE A DYNAMIC INPUT SETUP to make the inputs list flexible.
+=#
+println( " #--------------------------------------------------------------------------------")
+print(GREEN_FG(" # User inputs:\n"))
+println( " # Equation set:  ", inputs[:equation_set])
+println( " # Problem:       ", inputs[:problem])
+println( " # N. variables:  ", nvars)
+println( " # N. space dims: ", inputs[:nsd])
+println( " # N. x-points:   ", inputs[:npx])
+println( " # [xmin, xmax]:  ", inputs[:xmin], " ", inputs[:xmax])
+if (inputs[:nsd] > 1)
+    println( " # N. y-points:   ", inputs[:npy])
+    println( " # [ymin, ymax]:  ", inputs[:ymin], " ", inputs[:ymax])
+end
+if (inputs[:nsd] == 3)
+    println( " # N. z-points:   ", inputs[:npz])
+    println( " # [zmin, zmax]:  ", inputs[:zmin], " ", inputs[:zmax])
+end
 
-    # Create mesh
-    mod_mesh_build_mesh2d!(mesh)
-    #--------------------------------------------------------
-    # END Build mesh    
-    #--------------------------------------------------------
+#--------------------------------------------------------
+# Build mesh    
+#--------------------------------------------------------
+#
+# Initialize mesh struct
+mesh = St_mesh{TInt,TFloat}(zeros(inputs[:npx]), zeros(inputs[:npy]), zeros(inputs[:npz]),
+                            inputs[:xmin], inputs[:xmax],
+                            inputs[:ymin], inputs[:ymax],
+                            inputs[:zmin], inputs[:zmax],
+                            inputs[:npx], inputs[:npy], inputs[:npz])
 
-    #--------------------------------------------------------
-    # Initialize solution struct
-    #--------------------------------------------------------
-    #
-    # Initlaize solution struct
-   qs = St_solution{TInt,TFloat}(zeros(TFloat, nvars, inputs[:npx]*inputs[:npy]*inputs[:npz]),
-                                   zeros(TFloat, nvars, inputs[:npx]*inputs[:npy]*inputs[:npz]),
-                                   zeros(TFloat, nvars, inputs[:npx]*inputs[:npy]*inputs[:npz]),
-                                   zeros(TFloat, nvars, inputs[:npx]*inputs[:npy]*inputs[:npz]),
-                                   zeros(TFloat, nvars, inputs[:npx]*inputs[:npy]*inputs[:npz]),
-                                   zeros(TFloat, inputs[:nsd]*inputs[:nsd]))
+# Create mesh
+mod_mesh_build_mesh2d!(mesh)
+#--------------------------------------------------------
+# END Build mesh    
+#--------------------------------------------------------
+
+#--------------------------------------------------------
+# Initialize solution struct
+#--------------------------------------------------------
+#
+# Initlaize solution struct
+qsol = St_solution{TInt,TFloat}(zeros(TFloat, nvars, inputs[:npx]*inputs[:npy]*inputs[:npz]),
+                              zeros(TFloat, nvars, inputs[:npx]*inputs[:npy]*inputs[:npz]),
+                              zeros(TFloat, nvars, inputs[:npx]*inputs[:npy]*inputs[:npz]),
+                              zeros(TFloat, nvars, inputs[:npx]*inputs[:npy]*inputs[:npz]),
+                              zeros(TFloat, nvars, inputs[:npx]*inputs[:npy]*inputs[:npz]),
+                              zeros(TFloat, inputs[:nsd]*inputs[:nsd]))
+
+# Build initial conditions
+mod_solution_initial_conditions!(mesh,
+                                 qsol,
+                                 inputs[:nsd],
+                                 inputs[:npx], inputs[:npy], inputs[:npz],
+                                 inputs[:problem])
 
 
-    # Build initial conditions
-    mod_solution_initial_conditions!(mesh,
-                                     qs,
-                                     inputs[:nsd],
-                                     inputs[:npx], inputs[:npy], inputs[:npz],
-                                     inputs[:problem])
 
+plt = plot(mesh.x, qsol.q[1,:], w = 3)
+plot(scatter!(mesh.x, zeros(length(mesh.x)), x=:sepal_width, y=:sepal_length, mode="markers"))
+savefig("~/Work/Codes/jexpresso/figs/initial_conditions.png")
+
+#=mod_solution_solveODE!(mesh,
+                       qsol,
+                       inputs[:nsd],
+                       inputs[:npx], inputs[:npy], inputs[:npz],
+                       inputs[:problem])
+=#
+
+#=
+# Define a problem
+u[1] = 
+
+    p = (1.0,2.0,1.5,1.25) # a,b,c,d
+f = function (du,u,p,t) # Define f as an in-place update into du
+    a,b,c,d = p
+    du[1] = a*u[1] - b*u[1]*u[2]
+    du[2] = -c*u[2]+ d*u[1]*u[2]
+end
+u0 = [1.0;1.0]; tspan = (0.0,10.0)
+prob = ODEProblem(f,u0,tspan,p)
+
+# Solve the problem
+sol = DifferentialEquations.solve(prob);
+
+# Plot the solution using the plot recipe
+plot(sol,title="All Plots.jl Attributes are Available")
 
 #end #main
 #MPI.Barrier(comm)
 #MPI.Finalize()
+=#
--- src/solver/mod_solution.jl
+++ src/solver/mod_solution.jl
@@ -1,7 +1,9 @@
 include("../mesh/mod_mesh.jl")
 
-using Revise
+using DiffEqOperators, DifferentialEquations
 using LinearAlgebra
+using Revise
+using SparseMatricesCSR
 
 #Plots
 using Plots, DataFrames
@@ -29,22 +31,20 @@ function mod_solution_initial_conditions!(mesh::St_mesh,
                                           nsd,
                                           npx, npy, npz, 
                                           problem="burgers1d")
-    
+
     if (problem == "burgers1d" || problem == "burgers")
 
         if (nsd == 1)
+            qsol.q[1, 1] = 0
+            qsol.q[1, npx] = 0
             Δx = (mesh.xmax - mesh.xmin)/(npx-1)
-            for i = 1:npx
+            for i = 2:npx-1
                 x = mesh.x[i]
-                qsol.q[1, i] = sin(2π*x) + 0.5*sin(π*x)
+                qsol.q[1, i] = sin(2*π*x) + 0.5*sin(π*x)
             end
         end
     end
 
-    
-    plt = plot(mesh.x, qinit.q[1,:], w = 3)
-    plot(scatter!(mesh.x, zeros(length(mesh.x)), x=:sepal_width, y=:sepal_length, mode="markers"))
-    savefig("~/Work/Codes/jexpresso/figs/initial_conditions.png")
 end
 
 
@@ -54,8 +54,47 @@ function mod_solution_solveODE!(mesh::St_mesh,
                                 npx, npy, npz, 
                                 problem="burgers1d")
 
-    
-    
-    
-end
 
+    q0::TFloat
+    
+    # Define a problem
+    p = (1.0,2.0,1.5,1.25) # a,b,c,d
+    
+    ϵ  = 1.0e-2
+    Δx = (mesh.xmax - mesh.xmin)/(npx-1)
+    a  = ϵ/(Δx*Δx)
+    
+    x = mesh.x
+    qsol.q[1, 1]   = 0
+    qsol.q[1, npx] = 0
+    Δx = (mesh.xmax - mesh.xmin)/(npx-1)
+    q0 = sin(2*π*x) + 0.5*sin(π*x)
+
+   #= for i = 1:npx
+        for j = 1:npx
+        A[j, i] = 
+    end
+    
+        u = qsol.q
+    #    for i = 2:npx
+    #        rhs[i] = (ϵ/Δx)*(u[i+1] - 2.0*u[i] + u[i-1]) #- (0.25/Δx)*(u^2[i+1] - u^2[i-1])
+    #    end
+
+    =#
+    
+    # Plot the solution using the plot recipe
+    #plot(sol,title="All Plots.jl Attributes are Available")
+    
+  #=  f = function (du,u,p,t) # Define f as an in-place update into du
+
+        a,b,c,d = p
+        ϵ = 1.0e-2
+        Δx = (mesh.xmax - mesh.xmin)/(npx-1)
+        
+        for i=2:npx-1
+            RHS[i] = ϵ*()
+            du = a*u[1] - b*u[1]*u[2]
+        end
+    end
+    =#
+end
