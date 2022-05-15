92c0f680e9370e4ccafc31f90e28b961d24c2fcd
SIMONE MARRAS <simone@SIMONEs-MacBook-Air.local>
2022-05-15 13:12:43 -0400

    push

--- src/IO/user_inputs.jl
+++ src/IO/user_inputs.jl
@@ -8,8 +8,9 @@ function user_inputs()
         :lread_gmsh   => true,
         #:gmsh_filename => "./demo/gmsh_grids/hexa_UNSTR_coarse.msh",
         :gmsh_filename => "./demo/gmsh_grids/hexa_oneblock-2x1x1.msh",
+        #:gmsh_filename => "./demo/gmsh_grids/hexa_oneblock-1x1x1.msh",
         :nsd          => 3,           #number of space dimensions
-        :nop          => 3,           #Polynomila order        
+        :nop          => 2,           #Polynomila order        
         #:npx          => 10,         #N. points in x
         #:npy          => 10,           #N. points in y
         #:npz          => 5,           #N. points in z
