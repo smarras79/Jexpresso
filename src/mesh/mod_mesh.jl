aa978a7635454735b94b25d129515111cdc33755
Simone <smarras@njit.edu>
2022-08-25 12:15:38 -0400

    mod_mesh_mesh_driver: created wrapper t build-read the grid.

--- src/mesh/mod_mesh.jl
+++ src/mesh/mod_mesh.jl
@@ -827,3 +827,48 @@ function mod_mesh_build_mesh!(mesh::St_mesh)
     println(" # BUILD LINEAR CARTESIAN GRID ............................ DONE")
     
 end
+
+function mod_mesh_mesh_driver(inputs::Dict)
+    
+    if (haskey(inputs, :lread_gmsh) && inputs[:lread_gmsh]==true)
+        
+        println(" # Read gmsh grid and populate with high-order points ")
+        
+        # Initialize mesh struct: the arrays length will be increased in mod_mesh_read_gmsh
+        mesh = St_mesh{TInt,TFloat}(nsd=Int8(inputs[:nsd]),
+                                    nop=Int8(inputs[:nop]))
+        
+        # Read gmsh grid using the GridapGmsh reader
+        mod_mesh_read_gmsh!(mesh, inputs[:gmsh_filename])
+        
+        println(" # Read gmsh grid and populate with high-order points ........................ DONE")
+        
+    else
+        
+        println(" # Build native grid")
+
+        # Initialize mesh struct for native structured grid:
+        mesh = St_mesh{TInt,TFloat}(x = zeros(Int8(inputs[:npx])),
+                                    y = zeros(Int8(inputs[:npy])),
+                                    z = zeros(Int8(inputs[:npz])),
+                                    npx  = Int8(inputs[:npx]),
+                                    npy  = Int8(inputs[:npy]),
+                                    npz  = Int8(inputs[:npz]), 
+                                    xmin = Float64(inputs[:xmin]), xmax = Float64(inputs[:xmax]),
+                                    ymin = Float64(inputs[:ymin]), ymax = Float64(inputs[:ymax]),
+                                    zmin = Float64(inputs[:zmin]), zmax = Float64(inputs[:zmax]),
+                                    nsd=Int8(inputs[:nsd]),
+                                    nop=Int8(inputs[:nop]))
+
+        
+        
+        
+        #Write structured grid to VTK
+        #vtkfile = vtk_grid("mySTRUCTURED_GRID", mesh.x, mesh.y, mesh.z) # 3-D
+        #outfiles = vtk_save(vtkfile)
+        
+        println(" # Build navite grid ........................ DONE")
+    end
+
+    
+end
