c07d98092906141c67c3d5544b0d2aebb8a68b07
Simone <smarras@njit.edu>
2022-08-25 12:16:19 -0400

    Added a working 1D setup using the structures defined in Infrastructure/2D_3D_structures.jl

--- src/run.jl
+++ src/run.jl
@@ -24,6 +24,7 @@ include("./IO/mod_inputs.jl")
 include("./Mesh/mod_mesh.jl")
 include("./basis/basis_structs.jl")
 include("./Infrastructure/Kopriva_functions.jl")
+include("./Infrastructure/2D_3D_structures.jl")
 include("./solver/mod_solution.jl")
 #--------------------------------------------------------
 
@@ -42,53 +43,31 @@ inputs        = Dict{}()
 inputs, nvars = mod_inputs_user_inputs()
 
 #--------------------------------------------------------
-# Build mesh    
+# Create/read mesh
 #--------------------------------------------------------
-
-if (haskey(inputs, :lread_gmsh) && inputs[:lread_gmsh]==true)
-    
-    println(" # Read gmsh grid and populate with high-order points ")
-    
-    # Initialize mesh struct: the arrays length will be increased in mod_mesh_read_gmsh
-    mesh = St_mesh{TInt,TFloat}(nsd=Int8(inputs[:nsd]),
-                                nop=Int8(inputs[:nop]))
-    
-    # Read gmsh grid using the GridapGmsh reader
-    mod_mesh_read_gmsh!(mesh, inputs[:gmsh_filename])
-    
-    println(" # Read gmsh grid and populate with high-order points ........................ DONE")
-else
-    
-    println(" # Build native grid")
-
-    # Initialize mesh struct for native structured grid:
- #=   mesh = St_mesh{TInt,TFloat}(x = zeros(Int8(inputs[:npx])),
-                                y = zeros(Int8(inputs[:npy])),
-                                z = zeros(Int8(inputs[:npz])),
-                                npx  = Int8(inputs[:npx]),
-                                npy  = Int8(inputs[:npy]),
-                                npz  = Int8(inputs[:npz]), 
-                                xmin = Float64(inputs[:xmin]), xmax = Float64(inputs[:xmax]),
-                                ymin = Float64(inputs[:ymin]), ymax = Float64(inputs[:ymax]),
-                                zmin = Float64(inputs[:zmin]), zmax = Float64(inputs[:zmax]),
-                                nsd=Int8(inputs[:nsd]),
-                                nop=Int8(inputs[:nop]))
-    =#
-    
-    #@info mesh
-    nop=Int64(inputs[:nop])    
-    ElementMassMatrix(nop, nop, MassMatrix1D(), Float64)
-    
-
-    
-    #Write structured grid to VTK
-    #vtkfile = vtk_grid("mySTRUCTURED_GRID", mesh.x, mesh.y, mesh.z) # 3-D
-    #outfiles = vtk_save(vtkfile)
-    
-    println(" # Build navite grid ........................ DONE")
-end
+mod_mesh_mesh_driver(inputs)
 
 
 #--------------------------------------------------------
-# END Build mesh    
+# Build mass matrix
 #--------------------------------------------------------
+TInt=Int64
+TFloat=Float64
+
+N   = 6
+Nit = 100
+Tol = 0.1
+
+P1  = LGL1D()
+P2  = CGL1D()
+T1  = NodalGalerkin()
+T2  = Collocation()
+
+dim  = 1
+dims = [N]
+ND   = build_nodal_Storage(dims,P1,T1)
+
+
+#ElementMassMatrix(Int64(inputs[:nop]), Int64(inputs[:nop]), MassMatrix1D(), Float64)
+        
+
