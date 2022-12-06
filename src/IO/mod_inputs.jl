2bfdcdfcb4f7e150ea37b1c69954d6b57044b68c
Simone <smarras@njit.edu>
2022-12-06 14:56:54 -0500

    2D input for Yassine

--- src/IO/mod_inputs.jl
+++ src/IO/mod_inputs.jl
@@ -41,8 +41,9 @@ function mod_inputs_user_inputs()
     
     #Grid entries:
     if(!haskey(inputs, :lread_gmsh) || inputs[:lread_gmsh] == false)
+
         
-        mod_inputs_check(inputs, :nsd,  "e")
+        mod_inputs_check(inputs, :nsd,  Int8(1), "-")
         mod_inputs_check(inputs, :nelx, "e")
         mod_inputs_check(inputs, :xmin, "e")
         mod_inputs_check(inputs, :xmax, "e")
