8f958887a2cf0ad63afb1638078af6f5e5cc60d1
Simone <smarras@njit.edu>
2022-12-22 09:57:23 -0500

    wip. PULL

--- src/Problems/AbstractProblems.jl
+++ src/Problems/AbstractProblems.jl
@@ -4,4 +4,5 @@ struct AD1D <: AbstractProblem end
 struct NS1D <: AbstractProblem end
 struct Burgers1D <: AbstractProblem end
 struct Adv2D <: AbstractProblem end
+struct AD2D <: AbstractProblem end
 struct Heat2D <: AbstractProblem end
