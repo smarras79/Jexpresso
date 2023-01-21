1d310780647265d2ba363c9867df9a67dbf14eae
Simone <smarras@njit.edu>
2023-01-21 13:46:04 -0500

    pull

--- src/problems/AdvDiff/timeLoop.jl
+++ src/problems/AdvDiff/timeLoop.jl
@@ -22,11 +22,14 @@ function time_loop!(TD::RK5,
     t0 = t
 
     #Diffusion coefficient
+    it_interval = 10
     for it = 1:Nt
 
-        @printf "Solution at t=%.6f sec\n" t
-        t = t0 + Δt
-        t0 = t
+        if mod(it, it_interval) == 0
+            @printf "Solution at t=%.6f sec\n" t
+            t = t0 + Δt
+            t0 = t
+        end
         
         rk!(qp; TD, SD, QT, PT,
             mesh, metrics, basis, ω, M, Δt, inputs, T)
