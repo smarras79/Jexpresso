abd0247c3099a9b05bc9949bbc046aa3ff76103c
Simone <smarras@njit.edu>
2023-01-26 12:17:20 -0500

    Done and working: output written to ./src/problem/PROBLEM_NAME/output-timestamp

--- src/run.jl
+++ src/run.jl
@@ -19,28 +19,25 @@ const TFloat = Float64
 include("../src/io/mod_inputs.jl")
 parsed_args  = parse_commandline()
 problem_name = string(parsed_args["arg1"])
-problem_dir  = string("./problems/")
-driver_dir   = string(problem_dir, problem_name, "/drivers.jl")
-#--------------------------------------------------------
+problem_dir  = string("problems")
+driver_dir   = string("./", problem_dir, "/", problem_name, "/drivers.jl")
 include(driver_dir)
-
-mod_inputs_print_welcome()
 
 #--------------------------------------------------------
 #Read User Inputs:
 #--------------------------------------------------------
+mod_inputs_print_welcome()
 inputs        = Dict{}()
 inputs, nvars = mod_inputs_user_inputs!(problem_name, problem_dir)
 
 #--------------------------------------------------------
 #Create output directory if it doesn't exist:
-OUTPUT_DIR = string(problem_dir, problem_name, "/output-",  Dates.format(now(), "dduyyyy-HHMMSS"))
+#--------------------------------------------------------
+OUTPUT_DIR = string(dirname(@__DIR__()), "/src/", problem_dir, "/", problem_name, "/output-",  Dates.format(now(), "dduyyyy-HHMMSS"))
 if !isdir(OUTPUT_DIR)
-#if !ispath(string(OUTPUT_DIR))
-    @info "it doesnt exist " OUTPUT_DIR
- #   mkdir(joinpath(problem_dir, OUTPUT_DIR))
+    mkdir(OUTPUT_DIR)
 end
-error("assa")
+
 #--------------------------------------------------------
 # Problem setup
 # !!!!!!
