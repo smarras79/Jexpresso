1e9e79db8d5132ebc1d82f76ea6659a3777f058e
Simone <smarras@njit.edu>
2023-01-27 10:42:20 -0500

    LinearCLaw: adjusted build_rhs_diff for systems.

--- src/run.jl
+++ src/run.jl
@@ -33,10 +33,12 @@ inputs, nvars = mod_inputs_user_inputs!(problem_name, problem_dir)
 #--------------------------------------------------------
 #Create output directory if it doesn't exist:
 #--------------------------------------------------------
-OUTPUT_DIR = string(dirname(@__DIR__()), "/src/", problem_dir, "/", problem_name, "/output-",  Dates.format(now(), "dduyyyy-HHMMSS"))
+OUTPUT_DIR = string(dirname(@__DIR__()), "/src/", problem_dir, "/", problem_name, "/output-",  Dates.format(now(), "dduyyyy-HHMMSS/"))
 if !isdir(OUTPUT_DIR)
     mkdir(OUTPUT_DIR)
 end
+#Copy the user_inputs.jl used for the current run into the output directory:
+cp(string(dirname(@__DIR__()), "/src/", problem_dir, "/", problem_name, "/user_inputs.jl"), "user_inputs.jl.current", force=true)
 
 #--------------------------------------------------------
 # Problem setup
