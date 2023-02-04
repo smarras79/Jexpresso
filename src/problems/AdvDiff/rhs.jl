0fd0ae066db2e0b19219a5a8521935fa1a9c37ef
Simone <smarras@njit.edu>
2023-02-04 14:02:51 -0500

    modified bc and rhs to pass an array q[npoin,nvar] instead of the full struct of solution arrays.

--- src/problems/AdvDiff/rhs.jl
+++ src/problems/AdvDiff/rhs.jl
@@ -15,14 +15,9 @@ function build_rhs(SD::NSD_2D, QT, AP::AdvDiff, neqns, qp, ψ, dψ, ω, mesh::St
         for i=1:mesh.ngl
             for j=1:mesh.ngl
                 ip = mesh.connijk[i,j,iel]
-
-                q = qp.qn[ip,1]
                 
-                u = qp.qe[ip,2] #u,v are constant, hence stored in qp.qe instead of qp.qn
-                v = qp.qe[ip,3] #u,v are constant, hence stored in qp.qe instead of qp.qn
-                
-                F[i,j,iel,1] = q*u
-                G[i,j,iel,1] = q*v
+                F[i,j,iel,1] = qp[ip,1]*qp[ip,2]
+                G[i,j,iel,1] = qp[ip,1]*qp[ip,3]
                 
             end
         end
@@ -55,7 +50,6 @@ function build_rhs(SD::NSD_2D, QT, AP::AdvDiff, neqns, qp, ψ, dψ, ω, mesh::St
     return rhs_el
 end
 
-
 function build_rhs_diff(SD::NSD_2D, QT, AP::AdvDiff, nvars, qp, ψ, dψ, ω, νx, νy, mesh::St_mesh, metrics::St_metrics, T)
 
     N = mesh.ngl - 1
@@ -72,7 +66,7 @@ function build_rhs_diff(SD::NSD_2D, QT, AP::AdvDiff, nvars, qp, ψ, dψ, ω, νx
 
         for j=1:mesh.ngl, i=1:mesh.ngl
             m = mesh.connijk[i,j,iel]            
-            qnel[i,j,iel,1] = qp.qn[m,1]
+            qnel[i,j,iel,1] = qp[m,1]
         end
         
         for k = 1:mesh.ngl, l = 1:mesh.ngl
@@ -157,3 +151,106 @@ function build_rhs(SD::NSD_1D, QT::Exact, PT::Wave1D, mesh::St_mesh, metrics::St
     
     return rhs
 end
+
+#=
+
+function build_rhsold(SD::NSD_2D, QT, AP::AdvDiff, neqns, qp, ψ, dψ, ω, mesh::St_mesh, metrics::St_metrics, T)
+    
+    F    = zeros(mesh.ngl, mesh.ngl, mesh.nelem, 1)
+    G    = zeros(mesh.ngl, mesh.ngl, mesh.nelem, 1)
+    
+    rhs_el = zeros(mesh.ngl, mesh.ngl, mesh.nelem, neqns)
+    
+    for iel=1:mesh.nelem
+        for i=1:mesh.ngl
+            for j=1:mesh.ngl
+                ip = mesh.connijk[i,j,iel]
+
+                q = qp.qn[ip,1]
+                
+                u = qp.qe[ip,2] #u,v are constant, hence stored in qp.qe instead of qp.qn
+                v = qp.qe[ip,3] #u,v are constant, hence stored in qp.qe instead of qp.qn
+                
+                F[i,j,iel,1] = q*u
+                G[i,j,iel,1] = q*v
+                
+            end
+        end
+    end
+    
+    for ieq = 1:neqns
+        for iel=1:mesh.nelem
+            for i=1:mesh.ngl
+                for j=1:mesh.ngl
+                    
+                    dFdξ = dFdη = 0.0
+                    dGdξ = dGdη = 0.0
+                    for k = 1:mesh.ngl
+                        dFdξ = dFdξ + dψ[k, i]*F[k,j,iel,1]
+                        dFdη = dFdη + dψ[k, j]*F[i,k,iel,1]
+
+                        dGdξ = dGdξ + dψ[k, i]*G[k,j,iel,1]
+                        dGdη = dGdη + dψ[k, j]*G[i,k,iel,1]
+                    end
+                    dFdx = dFdξ*metrics.dξdx[i,j,iel] + dFdη*metrics.dηdx[i,j,iel]
+                    dGdy = dGdξ*metrics.dξdy[i,j,iel] + dGdη*metrics.dηdy[i,j,iel]
+                    
+                    rhs_el[i, j, iel, ieq] = -ω[i]*ω[j]*metrics.Je[i,j,iel]*(dFdx + dGdy)
+                end
+            end
+        end
+    end
+    #show(stdout, "text/plain", el_matrices.D)
+
+    return rhs_el
+end
+
+function build_rhs_diffold(SD::NSD_2D, QT, AP::AdvDiff, nvars, qp, ψ, dψ, ω, νx, νy, mesh::St_mesh, metrics::St_metrics, T)
+
+    N = mesh.ngl - 1
+    
+    qnel = zeros(mesh.ngl,mesh.ngl,mesh.nelem,3)
+    
+    rhsdiffξ_el = zeros(mesh.ngl,mesh.ngl,mesh.nelem)
+    rhsdiffη_el = zeros(mesh.ngl,mesh.ngl,mesh.nelem)
+    
+    #
+    # Add diffusion ν∫∇ψ⋅∇q (ν = const for now)
+    #
+    for iel=1:mesh.nelem
+
+        for j=1:mesh.ngl, i=1:mesh.ngl
+            m = mesh.connijk[i,j,iel]            
+            qnel[i,j,iel,1] = qp.qn[m,1]
+        end
+        
+        for k = 1:mesh.ngl, l = 1:mesh.ngl
+            ωJkl = ω[k]*ω[l]*metrics.Je[k, l, iel]
+            
+            dqdξ = 0.0
+            dqdη = 0.0
+            for i = 1:mesh.ngl
+                dqdξ = dqdξ + dψ[i,k]*qnel[i,l,iel,1]
+                dqdη = dqdη + dψ[i,l]*qnel[k,i,iel,1]
+            end
+            dqdx = dqdξ*metrics.dξdx[k,l,iel] + dqdη*metrics.dηdx[k,l,iel]
+            dqdy = dqdξ*metrics.dξdy[k,l,iel] + dqdη*metrics.dηdy[k,l,iel]
+            
+            ∇ξ∇q_kl = metrics.dξdx[k,l,iel]*dqdx + metrics.dξdy[k,l,iel]*dqdy
+            ∇η∇q_kl = metrics.dηdx[k,l,iel]*dqdx + metrics.dηdy[k,l,iel]*dqdy
+            
+            for i = 1:mesh.ngl
+                hll,     hkk     =  ψ[l,l],  ψ[k,k]
+                dhdξ_ik, dhdη_il = dψ[i,k], dψ[i,l]
+                
+                rhsdiffξ_el[i,l,iel] -= ωJkl*dhdξ_ik*hll*∇ξ∇q_kl
+                rhsdiffη_el[k,i,iel] -= ωJkl*hkk*dhdη_il*∇η∇q_kl
+            end
+        end
+    end
+
+    return (rhsdiffξ_el*νx + rhsdiffη_el*νy)
+    
+end
+
+=#
