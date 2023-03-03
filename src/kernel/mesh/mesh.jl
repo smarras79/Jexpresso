d94d1f3099b600349fcd8fddaafe801d0a53e59a
Simone <smarras@njit.edu>
2023-03-03 10:17:52 -0500

    minor

--- src/kernel/mesh/mesh.jl
+++ src/kernel/mesh/mesh.jl
@@ -482,7 +482,7 @@ if mesh.nsd == 2
         if isboundary_edge[iedge] == true
             for igl = 1:mesh.ngl
                 mesh.poin_in_bdy_edge[iedge_bdy, igl] = mesh.poin_in_edge[iedge, igl]
-                mesh.bdy_edge_type[iedge_bdy] = "ciao" #mesh.edge_type[iedge]
+                mesh.bdy_edge_type[iedge_bdy] = mesh.edge_type[iedge]
             end
             iedge_bdy += 1
         end
