4509c58c7d85ad5503429b7453ff1f8f58162186
Simone <smarras@njit.edu>
2023-03-23 12:26:00 -0400

    push src/kernel/operators/rhs.jl

--- src/problems/AdvDiff/user_inputs.jl
+++ src/problems/AdvDiff/user_inputs.jl
@@ -24,7 +24,7 @@ function user_inputs()
         #---------------------------------------------------------------------------
         # Mesh paramters and files:
         #---------------------------------------------------------------------------
-        :lread_gmsh          => true, #If false, a 1D problem will be enforced
+        #:lread_gmsh          => true, #If false, a 1D problem will be enforced
 
         #:gmsh_filename       => "./meshes/gmsh_grids/2d-grid.msh", 
         #:gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_25x25.msh",
