4caa5f09f09817c1de4b4fa0a56ee555f15b6c5e
Simone <smarras@njit.edu>
2023-04-30 15:45:40 -0400

    CompEuler for rtb case works (or so it seems!).

--- src/io/write_output.jl
+++ src/io/write_output.jl
@@ -180,20 +180,24 @@ function write_vtk(SD::NSD_2D, mesh::St_mesh, q::Array, title::String, OUTPUT_DI
     ivar = 3
     idx = (ivar - 1)*npoin
     qout[idx+1:3*npoin] = q[idx+1:3*npoin]./q[1:npoin]
-     
+
     if case === "rtb"
-            
+
+        outvars = ("rho", "u", "v", "theta")
+        
         if (inputs[:loutput_pert] == true && size(qexact, 1) === npoin)
 
             #ρ'
             qout[1:npoin] = q[1:npoin] .- qexact[1:npoin,1]
             
-            #θ' = (ρθ - ρθref)/ρ = ρθ/ρ - ρθref/ρ
+            #θ' = (ρθ - ρθref)/ρ = ρθ/ρ - ρrefθref/ρref
             ivar = 4
             idx = (ivar - 1)*npoin
             qout[idx+1:4*npoin] = q[idx+1:4*npoin]./q[1:npoin] .- qexact[1:npoin,4]./qexact[1:npoin,1]
            
         else
+            outvars = ("rho", "u", "v", "E")
+            
             #E = ρE/ρ
             idx = 4*npoin
             qout[idx+1:4*npoin] .= (q[2*npoin+1:4*npoin] .- 0.5*(q[npoin+1:2*npoin].*q[npoin+1:2*npoin] .+ q[npoin+1:3*npoin].*q[npoin+1:3*npoin])./q[1:npoin])./q[1:npoin] #internal energy: p/((γ-1)ρ)
@@ -214,10 +218,10 @@ function write_vtk(SD::NSD_2D, mesh::St_mesh, q::Array, title::String, OUTPUT_DI
     fout_name = string(OUTPUT_DIR, "/rtb_it", iout, ".vtu")
     
     vtkfile = vtk_grid(fout_name, mesh.x[1:npoin], mesh.y[1:npoin], mesh.y[1:npoin]*0.0, cells)
-    vtkfile["rho",   VTKPointData()] =  qout[1:npoin]
-    vtkfile["u",     VTKPointData()] =  qout[npoin+1:2*npoin]
-    vtkfile["v",     VTKPointData()] =  qout[2*npoin+1:3*npoin]
-    vtkfile["theta", VTKPointData()] =  qout[3*npoin+1:4*npoin]
+    for ivar = 1:nvar
+        idx = (ivar - 1)*npoin
+        vtkfile[string(outvars[ivar]), VTKPointData()] =  qout[idx+1:ivar*npoin]
+    end
     outfiles = vtk_save(vtkfile)
     
 end
