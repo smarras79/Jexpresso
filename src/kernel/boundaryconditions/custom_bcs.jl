9af61c7e77c208b7215e3fdc143ddbee3633a50d
Simone <smarras@njit.edu>
2023-05-10 11:31:48 -0400

    fixed small bug in custom_bcs.jl

--- src/kernel/boundaryconditions/custom_bcs.jl
+++ src/kernel/boundaryconditions/custom_bcs.jl
@@ -11,28 +11,28 @@ end
 include(user_bc_dir)
 #---------------------------------------------------------------------------
 
-function neumann(q,gradq,x,y,t, mesh, metrics,tag)
+function neumann(q, gradq, x, y, t, mesh, metrics, tag)
     
-    rhs = user_bc_neumann(q,gradq,x,y,t,tag)
+    rhs = user_bc_neumann(q, gradq, x, y, t, tag)
     return rhs
 end
 
-function neumann(q,gradq,x,t, mesh, metrics)
+function neumann(q, gradq, x, t, mesh, metrics)
 
-    rhs = user_bc_neumann(q,gradq,x,t)
+    rhs = user_bc_neumann(q, gradq, x, t)
     return rhs
 end
 
-function dirichlet!(q,gradq,x,y,t, mesh, metrics,tag,qbdy)
+function dirichlet!(q, x, y, t, mesh, metrics, tag, qbdy)
 
-    qbdy = user_bc_dirichlet!(q,gradq,x,y,t,tag,qbdy)
+    qbdy = user_bc_dirichlet!(q, x, y, t, tag, qbdy)
     
     return qbdy
 end
 
-function dirichlet!(q,gradq,x,t, mesh, metrics,qbdy)
+function dirichlet!(q, x, t, mesh, metrics, qbdy)
 
-    qbdy = user_bc_dirichlet!(q,gradq,x,t)
+    qbdy = user_bc_dirichlet!(q, x, t)
 
     return qbdy
 end
