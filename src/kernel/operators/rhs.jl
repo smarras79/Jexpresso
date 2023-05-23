84286df82e5fb06a83467638f8ca9853e499d08e
Simone <simone@Simones-MacBook-Air.local>
2023-05-23 11:02:15 -0400

    push

--- src/kernel/operators/rhs.jl
+++ src/kernel/operators/rhs.jl
@@ -72,41 +72,58 @@ function build_rhs(SD::NSD_1D, QT::Inexact, PT::AdvDiff, qp::Array, neqs, basis,
                    qnm1=zeros(Float64,1,1), qnm2=zeros(Float64,1,1), μ=zeros(Float64,1,1))
 
     F      = zeros(mesh.ngl, mesh.nelem, neqs)
-    dFdξ   = zeros(neqs)
     rhs_el = zeros(mesh.ngl, mesh.nelem, neqs)
     qq     = zeros(mesh.npoin, neqs)
     for i=1:neqs
         idx = (i-1)*mesh.npoin
         qq[:,i] .= qp[idx+1:i*mesh.npoin]
     end    
-    Fuser = user_flux(T, SD, qq, mesh; neqs=neqs)
-
+    
     for iel=1:mesh.nelem
         
-        Jac     = mesh.Δx[iel]/2
-        metrics = 2.0/mesh.Δx[iel]
+        Jac  = mesh.Δx[iel]/2
+        dξdx = 2.0/mesh.Δx[iel]
         
         for i=1:mesh.ngl
             ip = mesh.conn[i,iel]
-            F[i,iel,1:neqs] .= Fuser[ip,1:neqs]
+            F[i,iel,1:neqs] .= user_flux(T, SD, qq[ip,1:neqs], mesh; neqs=neqs)
         end
 
-        for i=1:mesh.ngl
-            dFdξ[1:neqs] .= 0.0
-            for k = 1:mesh.ngl
-                dFdξ[1:neqs] .= dFdξ[1:neqs] .+ basis.dψ[k,i]*F[k, iel, 1:neqs]*metrics
+        for ieq = 1:neqs
+            for i=1:mesh.ngl
+                dFdξ = 0.0
+                for k = 1:mesh.ngl
+                    dFdξ += basis.dψ[k,i]*F[k,iel,ieq]*dξdx
+                end
+                rhs_el[i,iel,ieq] += ω[i]*Jac*dFdξ
             end
-            rhs_el[i, iel, 1:neqs] .+= ω[i]*Jac*dFdξ[1:neqs]
         end
     end
     
     RHS = DSS_rhs(SD, rhs_el, mesh.conn, mesh.nelem, mesh.npoin, neqs, mesh.nop, T)
-    divive_by_mass_matrix!(RHS, M, QT, neqs)
     
     for i=1:neqs
         idx = (i-1)*mesh.npoin
         qp[idx+1:i*mesh.npoin] .= qq[:,i]
     end
+    if (inputs[:lvisc] == true)
+        
+        if (inputs[:visc_model] === "dsgs")
+            
+            if (rem(time, Δt) == 0 && time > 0.0)
+                qnm1 .= qnm2
+                qnm2 .= qq
+            end
+            
+            compute_viscosity!(μ, SD, PT, qq, qnm1, qnm2, RHS, Δt, mesh, metrics, T)
+        else
+            μ[:] .= inputs[:νx]
+        end
+        rhs_diff_el = build_rhs_diff(SD, QT, PT, qp, neqs, basis, ω, inputs, mesh, metrics, μ, T;)
+        RHS .= RHS .+ DSS_rhs(SD, rhs_diff_el, mesh.connijk, mesh.nelem, mesh.npoin, neqs, mesh.nop, T)
+    end
+
+    
     apply_periodicity!(SD, RHS, qp, mesh, inputs, QT, metrics, basis.ψ, basis.dψ, ω, 0, neqs)
     
     return RHS
@@ -521,17 +538,16 @@ function build_rhs(SD::NSD_1D, QT::Inexact, PT::CompEuler, qp::Array, neqs, basi
     F      = zeros(T, mesh.ngl,mesh.nelem, neqs)
     rhs_el = zeros(T, mesh.ngl,mesh.nelem, neqs)
     qq     = zeros(T, mesh.npoin,neqs)
-    
     for i=1:neqs
         idx = (i-1)*mesh.npoin
-        qq[:,i] .= qp[idx+1:i*mesh.npoin]
+        qq[:,i] .= 0.0 .+ view(qp, idx+1:i*mesh.npoin)
     end
     
     for iel=1:mesh.nelem
         dξdx = 2.0/mesh.Δx[iel]
         for i=1:mesh.ngl
             ip = mesh.conn[i,iel]
-            F[i,iel,1:neqs] = user_flux(T, SD, qq[ip,1:neqs], mesh; neqs=neqs)
+            F[i,iel,1:neqs] .= user_flux(T, SD, qq[ip,1:neqs], mesh; neqs=neqs)
         end
         
         for ieq = 1:neqs
