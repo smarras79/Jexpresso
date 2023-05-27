2263dff80ad8db996da7bc776efe20c54c1a4213
Simone <simone@Simones-MacBook-Air.local>
2023-05-27 09:55:15 -0400

    ADvDiff 1D now uses the same rhs of CompEuler 1D

--- src/equations/AdvDiff/case1/user_flux.jl
+++ src/equations/AdvDiff/case1/user_flux.jl
@@ -2,15 +2,3 @@ function user_flux(T, SD::NSD_1D, q::Array, mesh::St_mesh; neqs=1)
     
     return +1.0*q[1]
 end
-
-#=function user_flux(T, SD::NSD_1D, q::Array, mesh::St_mesh; neqs=1)
-    #
-    # F(q(x)) = 0.8*q
-    #
-    F = zeros(T, mesh.npoin, 1:neqs)
-    
-    F[1] .= +1.0*q[:, 1]
-    
-    return F
-    
-end=#
