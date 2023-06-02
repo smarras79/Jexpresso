e6ed73bb85cf8e0cfae151c1684cface599f9915
Simone <simone@Simones-MacBook-Air.local>
2023-06-02 10:18:37 -0400

    wip: more optimizing with mutable user_flux!

--- src/equations/CompEuler/theta/user_flux.jl
+++ src/equations/CompEuler/theta/user_flux.jl
@@ -1,10 +1,10 @@
-function user_flux!(F, G, T, SD::NSD_2D, q::Array, mesh::St_mesh; neqs=4)
+function user_flux(T, SD::NSD_2D, q::Array, mesh::St_mesh; neqs=4)
 
     PhysConst = PhysicalConst{Float64}()
     
-    #F = zeros(neqs)
-    #G = zeros(neqs)
-            
+    F = zeros(neqs)
+    G = zeros(neqs)
+    
     ρ  = q[1]
     ρu = q[2]
     ρv = q[3]
@@ -18,6 +18,60 @@ function user_flux!(F, G, T, SD::NSD_2D, q::Array, mesh::St_mesh; neqs=4)
     F[2] = ρu*u + Press
     F[3] = ρv*u
     F[4] = ρθ*u
+    
+    G[1] = ρv
+    G[2] = ρu*v
+    G[3] = ρv*v + Press
+    G[4] = ρθ*v
+
+    return F, G
+end
+
+
+function user_fluxF!(F::Array, T, SD::NSD_2D, q::Array, mesh::St_mesh; neqs=4)
+
+    PhysConst = PhysicalConst{Float64}()
+    
+    ρ  = q[1]
+    ρu = q[2]
+    ρv = q[3]
+    ρθ = q[4]
+    θ  = ρθ/ρ
+    u  = ρu/ρ
+    v  = ρv/ρ
+    
+    Press = perfectGasLaw_ρθtoP(PhysConst, ρ=ρ, θ=θ)
+    F[1] = ρu
+    F[2] = ρu*u + Press
+    F[3] = ρv*u
+    F[4] = ρθ*u
+    
+    #G[1] = ρv
+    #G[2] = ρu*v
+    #G[3] = ρv*v + Press
+    #G[4] = ρθ*v
+end
+
+function user_fluxG!(G::Array, T, SD::NSD_2D, q::Array, mesh::St_mesh; neqs=4)
+
+    PhysConst = PhysicalConst{Float64}()
+    
+    #F = zeros(neqs)
+    #G = zeros(neqs)
+    
+    ρ  = q[1]
+    ρu = q[2]
+    ρv = q[3]
+    ρθ = q[4]
+    θ  = ρθ/ρ
+    u  = ρu/ρ
+    v  = ρv/ρ
+    
+    Press = perfectGasLaw_ρθtoP(PhysConst, ρ=ρ, θ=θ)
+    #F[1] = ρu
+    #F[2] = ρu*u + Press
+    #F[3] = ρv*u
+    #F[4] = ρθ*u
     
     G[1] = ρv
     G[2] = ρu*v
