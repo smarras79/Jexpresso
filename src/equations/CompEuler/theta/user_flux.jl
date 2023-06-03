d06b7628ad6fced7e9333c00a79232556b347202
Simone <simone@Simones-MacBook-Air.local>
2023-06-03 11:46:48 -0400

    push

--- src/equations/CompEuler/theta/user_flux.jl
+++ src/equations/CompEuler/theta/user_flux.jl
@@ -28,7 +28,7 @@ function user_flux(T, SD::NSD_2D, q::Array, mesh::St_mesh; neqs=4)
 end
 
 
-function user_fluxF!(F::Array, T, SD::NSD_2D, q::Array, mesh::St_mesh; neqs=4)
+function user_flux!(F::Array, G::Array, T, SD::NSD_2D, q::Array, i,j, ip, mesh::St_mesh; neqs=4)
 
     PhysConst = PhysicalConst{Float64}()
     
@@ -41,15 +41,15 @@ function user_fluxF!(F::Array, T, SD::NSD_2D, q::Array, mesh::St_mesh; neqs=4)
     v  = ρv/ρ
     
     Press = perfectGasLaw_ρθtoP(PhysConst, ρ=ρ, θ=θ)
-    F[1] = ρu
-    F[2] = ρu*u + Press
-    F[3] = ρv*u
-    F[4] = ρθ*u
+    F[i,j,1] = ρu
+    F[i,j,2] = ρu*u + Press
+    F[i,j,3] = ρv*u
+    F[i,j,4] = ρθ*u
     
-    #G[1] = ρv
-    #G[2] = ρu*v
-    #G[3] = ρv*v + Press
-    #G[4] = ρθ*v
+    G[i,j,1] = ρv
+    G[i,j,2] = ρu*v
+    G[i,j,3] = ρv*v + Press
+    G[i,j,4] = ρθ*v
 end
 
 function user_fluxG!(G::Array, T, SD::NSD_2D, q::Array, mesh::St_mesh; neqs=4)
