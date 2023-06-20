973541735df89a6b2aacdeb5476bef97ea5abd2a
Simone <simone@Simones-MacBook-Air.local>
2023-06-20 15:18:15 +0200

    wip

--- src/equations/AdvDiff/case1/user_source.jl
+++ src/equations/AdvDiff/case1/user_source.jl
@@ -2,18 +2,16 @@ function user_source!(S::SubArray{Float64}, q::SubArray{Float64}, npoin; neqs=1,
     
     #
     # clateral
-    
-    cs=0.5
-    nsponge_points = 30
+    nsponge_points = 12
     
     # distance from the boundary. xs in Restelli's thesis
     dsx = (xmax - xmin)/(nelx*(ngl - 1)) # equivalent grid spacing
     dbl = min(x - xmin, xmax - x)
     
     beta_coe =  1.0 - tanh(dbl/(nsponge_points * dsx))
-    cside= cs*beta_coe
+    cside= beta_coe
     
     #@info "β x: " beta_coe x
-    S .= -cside*q
+    S .= -(1.0 - cside).*q
     
 end
