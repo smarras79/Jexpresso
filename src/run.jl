67100f1fe93b7c88838f5a59db0334de214ec140
smarras79 <smarras@njit.edu>
2023-08-31 14:11:20 -0400

    cleanup in run.jl

--- src/run.jl
+++ src/run.jl
@@ -7,55 +7,48 @@
 # 2. Push equations name to ARGS
 #    You need this only when you run a new equations
 #
-#    julia > push!(empty!(ARGS), EQUATIONS::String);
-#    julia > include(./src/run.jl)
+#    julia > push!(empty!(ARGS), EQUATIONS::String, EQUATIONS_CASE_NAME::String);
+#    julia > include(./src/Jexpresso.jl)
 #
-# EQUATIONS is the name of your equations directory
-# as $JEXPRESSO/src/equations/equations
+#    EQUATIONS is the name of your equations directory as $JEXPRESSO/src/equations/EQUATIONS
+#    EQUATIONS_CASE_NAME is the name of the subdirectory $JEXPRESSO/src/equations/EQUATIONS_CASE_NAME
 #
-# Ex. If you run the Advection Diffusion equations in $JEXPRESSO/src/equations/AdvDiff
+# Ex. To run the Compressible Euler equations in $JEXPRESSO/src/equations/CompEuler/theta
 # 
-#  julia > push!(empty!(ARGS), "AdvDiff");
-#  julia > include(./src/run.jl)
+#  julia > push!(empty!(ARGS), "CompEuler", "theta");
+#  julia > include(./src/Jexpresso.jl)
 #
 #--------------------------------------------------------
-if isempty(ARGS)
-    s = """
-            
-            Please, run the following every time that EQUATIONS changes:
-                julia> push!(empty!(ARGS), EQUATIONS::String, , EQUATIONS_CASE_NAME::String);
+function myparsing()
+    if size(ARGS, 1) < 2
+        println("")
+        println(" ERROR: Missing command line arguments")
+        println(" Usage example:")
+        println("     julia> push!(empty!(ARGS), \"CompEuler\", \"theta\")")
+        println("     julia> include(./src/Jexpresso.jl)")
+        println(" -- See detailed instructions in the README file on github.")
+        println("")
+    else
+        include("./io/mod_inputs.jl")
 
-            and only then run jexpresso with:
-                julia> include(./src/run.jl)
-
-            Currently avaiable EQUATIONS options:
-            - Elliptic
-            - AdvDiff
-
-            EQUATIONS_CASE_NAME is user defined and must be the name of the case directory inside EQUATIONS:
-                For example, if "AdvDiff" contains a directory called "Case1", you would do the following:
-                    julia> push!(empty!(ARGS), "AdvDiff", "Case1");
-                but if you don't have any CASE DIRECTORY inside "AdvDiff", then you simply 
-                    julia> push!(empty!(ARGS), "AdvDiff");
-        """
-    error(s)
+        #parsed_args  = parse_commandline()
+        s = ArgParseSettings()
+        parsed_args = parse_args(s)
+        equations = string(parsed_args["arg1"])
+        if (parsed_args["arg2"] === nothing)
+            equations_case_name = ""
+        else
+            equations_case_name = string(parsed_args["arg2"])
+        end
+        equations_dir  = string("equations")
+        driver_dir   = string("./", equations_dir, "/", equations, "/", equations_case_name, "/drivers.jl")
+        include(driver_dir)
+    end
 end
-
-include("./io/mod_inputs.jl")
-parsed_args  = parse_commandline()
-equations = string(parsed_args["arg1"])
-if (parsed_args["arg2"] === nothing)
-    equations_case_name = ""
-else
-    equations_case_name = string(parsed_args["arg2"])
-end
-equations_dir  = string("equations")
-driver_dir   = string("./", equations_dir, "/", equations, "/", equations_case_name, "/drivers.jl")
-include(driver_dir)
-
 #--------------------------------------------------------
 #Read User Inputs:
 #--------------------------------------------------------
+myparsing()
 mod_inputs_print_welcome()
 inputs        = Dict{}()
 inputs        = mod_inputs_user_inputs!(equations, equations_case_name, equations_dir)
