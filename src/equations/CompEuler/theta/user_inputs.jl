ecf5bb9a7d69d3d4dededb1cf59dd67d133fe4be
smarras79 <smarras@njit.edu>
2023-09-10 15:34:42 -0400

    push to test on cluster

--- src/equations/CompEuler/theta/user_inputs.jl
+++ src/equations/CompEuler/theta/user_inputs.jl
@@ -3,9 +3,9 @@ function user_inputs()
         #---------------------------------------------------------------------------
         # User define your inputs below: the order doesn't matter
         #---------------------------------------------------------------------------
-        :tend                 => 100,
-        :ode_solver           => SSPRK54(), #13.8s dt=0.35
-        :Δt                   => 0.4,
+        :tend                 => 300,
+        :ode_solver           => SSPRK54(), #ORK256(),#SSPRK33(), #SSPRK33(), #MSRK5(), #SSPRK54(),
+        :Δt                   => 0.2,
         :ndiagnostics_outputs => 2,
         :case                 => "rtb",
         #---------------------------------------------------------------------------
@@ -18,7 +18,7 @@ function user_inputs()
         #---------------------------------------------------------------------------
         # Physical parameters/constants:
         #---------------------------------------------------------------------------
-        :lvisc                => true, #false by default NOTICE: works only for Inexact
+        #:lvisc                => true, #false by default NOTICE: works only for Inexact
         :νx                   => 30.0, #horizontal viscosity constant for momentum
         :νy                   => 30.0, #vertical   viscosity constant for momentum
         :κ                    => 60.0, #kinematic viscosity constant for θ equation
