d5bff93b41c28780179ea04c0b76bcaa82614f5a
Simone <smarras@njit.edu>
2023-09-15 08:30:17 -0400

    added transformation of q for NCL in write_output.jl and VTK writer

--- src/io/write_output.jl
+++ src/io/write_output.jl
@@ -117,7 +117,7 @@ function write_output(sol::SciMLBase.LinearSolution, SD::NSD_2D, mesh::St_mesh, 
     println(string(" # Writing output to ASCII file:", OUTPUT_DIR, "*.dat ...  DONE") )
 end
 
-function write_output(sol::SciMLBase.LinearSolution, SD::NSD_2D, mesh::St_mesh, OUTPUT_DIR::String, inputs::Dict, outformat::VTK; nvar=1)
+function write_output(sol::SciMLBase.LinearSolution, SD::NSD_2D, CL, mesh::St_mesh, OUTPUT_DIR::String, inputs::Dict, outformat::VTK; nvar=1)
     
     println(string(" # Writing output to VTK file:", OUTPUT_DIR, "*.vtu ...  ") )
     for iout = 1:inputs[:ndiagnostics_outputs]
@@ -131,7 +131,7 @@ end
 #------------
 # VTK writer
 #------------
-function write_vtk(SD::NSD_2D, mesh::St_mesh, q::Array, title::String, OUTPUT_DIR::String, inputs::Dict; iout=1, nvar=1, qexact=zeros(1,nvar), case="")
+function write_vtk(SD::NSD_2D, CL, mesh::St_mesh, q::Array, title::String, OUTPUT_DIR::String, inputs::Dict; iout=1, nvar=1, qexact=zeros(1,nvar), case="")
     #nothing
     
     subelem = Array{Int64}(undef, mesh.nelem*(mesh.ngl-1)^2, 4)
@@ -167,49 +167,74 @@ function write_vtk(SD::NSD_2D, mesh::St_mesh, q::Array, title::String, OUTPUT_DI
     #ρ
     qout[1:npoin] = q[1:npoin]
 
-    #u = ρu/ρ
-    ivar = 2
-    idx = (ivar - 1)*npoin
-    qout[idx+1:2*npoin] = q[idx+1:2*npoin]./q[1:npoin]
+    if (CL == CL())
+        #u = ρu/ρ
+        ivar = 2
+        idx = (ivar - 1)*npoin
+        qout[idx+1:2*npoin] = q[idx+1:2*npoin]./q[1:npoin]
 
-    #v = ρv/ρ
-    ivar = 3
-    idx = (ivar - 1)*npoin
-    qout[idx+1:3*npoin] = q[idx+1:3*npoin]./q[1:npoin]
+        #v = ρv/ρ
+        ivar = 3
+        idx = (ivar - 1)*npoin
+        qout[idx+1:3*npoin] = q[idx+1:3*npoin]./q[1:npoin]
 
-    if case === "rtb"
+        if case === "rtb"
 
-        outvars = ("rho", "u", "v", "theta")
-        
-        if (inputs[:loutput_pert] == true && size(qexact, 1) === npoin)
-
-            #ρ'
-            qout[1:npoin] = q[1:npoin] .- qexact[1:npoin,1]
+            outvars = ("rho", "u", "v", "theta")
             
-            #θ' = (ρθ - ρθref)/ρ = ρθ/ρ - ρrefθref/ρref
-            ivar = 4
-            idx = (ivar - 1)*npoin
-            qout[idx+1:4*npoin] = q[idx+1:4*npoin]./q[1:npoin] .- qexact[1:npoin,4]./qexact[1:npoin,1]
-           
-        else
-            outvars = ("rho", "u", "v", "E")
+            if (inputs[:loutput_pert] == true && size(qexact, 1) === npoin)
+
+                #ρ'
+                qout[1:npoin] = q[1:npoin] .- qexact[1:npoin,1]
+                
+                #θ' = (ρθ - ρθref)/ρ = ρθ/ρ - ρrefθref/ρref
+                ivar = 4
+                idx = (ivar - 1)*npoin
+                qout[idx+1:4*npoin] = q[idx+1:4*npoin]./q[1:npoin] .- qexact[1:npoin,4]./qexact[1:npoin,1]
+                
+            else
+                outvars = ("rho", "u", "v", "E")
+                
+                #E = ρE/ρ
+                idx = 4*npoin
+                qout[idx+1:4*npoin] .= (q[2*npoin+1:4*npoin] .- 0.5*(q[npoin+1:2*npoin].*q[npoin+1:2*npoin] .+ q[npoin+1:3*npoin].*q[npoin+1:3*npoin])./q[1:npoin])./q[1:npoin] #internal energy: p/((γ-1)ρ)
+            end
+        end
+    elseif(CL == NCL())
+        #u = ρu/ρ
+        ivar = 2
+        idx = (ivar - 1)*npoin
+        qout[idx+1:2*npoin] = q[idx+1:2*npoin]
+
+        #v = ρv/ρ
+        ivar = 3
+        idx = (ivar - 1)*npoin
+        qout[idx+1:3*npoin] = q[idx+1:3*npoin]
+
+        if case === "rtb"
+
+            outvars = ("rho", "u", "v", "theta")
             
-            #E = ρE/ρ
-            idx = 4*npoin
-            qout[idx+1:4*npoin] .= (q[2*npoin+1:4*npoin] .- 0.5*(q[npoin+1:2*npoin].*q[npoin+1:2*npoin] .+ q[npoin+1:3*npoin].*q[npoin+1:3*npoin])./q[1:npoin])./q[1:npoin] #internal energy: p/((γ-1)ρ)
+            if (inputs[:loutput_pert] == true && size(qexact, 1) === npoin)
+
+                #ρ'
+                qout[1:npoin] = q[1:npoin] .- qexact[1:npoin,1]
+                
+                #θ' = (ρθ - ρθref)/ρ = ρθ/ρ - ρrefθref/ρref
+                ivar = 4
+                idx = (ivar - 1)*npoin
+                qout[idx+1:4*npoin] = q[idx+1:4*npoin].- qexact[1:npoin,4]
+                
+            else
+                outvars = ("rho", "u", "v", "E")
+                
+                #E = ρE/ρ
+                idx = 4*npoin
+                qout[idx+1:4*npoin] .= (q[2*npoin+1:4*npoin] .- 0.5*(q[npoin+1:2*npoin].*q[npoin+1:2*npoin] .+ q[npoin+1:3*npoin].*q[npoin+1:3*npoin])./q[1:npoin])./q[1:npoin] #internal energy: p/((γ-1)ρ)
+            end
         end
     end
     
-    #=for ivar=1:nvar
-        idx = (ivar - 1)*npoin
-        
-        fout_name = string(OUTPUT_DIR, "/ivar", ivar, "-it", iout, ".vtu")
-    
-        vtk_grid(fout_name, mesh.x[1:npoin], mesh.y[1:npoin], mesh.y[1:npoin]*0.0, cells) do vtk
-            vtk[string("q", ivar), VTKPointData()] = qout[idx+1:ivar*npoin]
-        end
-    end=#
-     
     
     fout_name = string(OUTPUT_DIR, "/rtb_it", iout, ".vtu")
     
