44e3d644e6dbc6bc0582018caa7a16c2f378f2db
smarras79 <smarras@njit.edu>
2023-10-05 14:53:50 -0400

    Cleanup of problems/equations. This branch now only contains CompEuler. A usr can add new cases starting from this general setup of conservatio laws.

--- problems/equations/AdvDiff/2d/user_source.jl
+++
@@ -1,7 +0,0 @@
-function user_source!(S::SubArray{Float64}, q::SubArray{Float64}, npoin::Int64; neqs=1)
-    #
-    # S(q(x)) = 0.0
-    #
-    S[1] = 0.0
-    
-end
--- problems/equations/ShallowWater/2d/user_source.jl
+++
@@ -1,6 +0,0 @@
-#Rain
-function user_source(T, q::Array, npoin::Int64, x::Array)
-
-    nothing
-    
-end
