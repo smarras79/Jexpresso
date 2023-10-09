59328de2bad8aedf6433ae91cd64469110687f67
smarras79 <smarras@njit.edu>
2023-10-09 12:37:07 -0400

    push docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,18 @@
 using Documenter, Jexpresso
 
-pages = [
-  "Home" => "index.md",
- ]
-
-makedocs(;
+makedocs(
+    sitename="Jexpresso.jl",
     modules=[Jexpresso],
     format=Documenter.HTML(),
-    pages=pages,
+    pages = Any[
+        "Home" => "index.md",
+    ],
     repo="https://github.com/smarras79/Jexpresso.jl/blob/{commit}{path}#L{line}",
-    sitename="Jexpresso.jl",
-    authors="S. Marras <smarras@njit.edu>, Y. Tissaoui <yt277@njit.edu>",
+    authors="S. Marras <smarras@njit.edu>, Y. Tissaoui <yt277@njit.edu>"
     # warnonly=true, # for debugging
 )
 
-deploydocs(;
-    repo="github.com/smarras79/Jexpresso.jl",
+deploydocs(
+    repo="https://github.com/smarras79/Jexpresso.jl/blob/{commit}{path}#L{line}",
+#           repo="github.com/smarras79/Jexpresso.jl",
 )
