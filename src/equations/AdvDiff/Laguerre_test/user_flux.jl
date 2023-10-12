fe71ed017d5958428896b04d31afc2015a80725e
Yassine <yassinetissaoui@gmail.com>
2023-10-12 13:49:58 -0400

    removed extra include calls

--- src/equations/AdvDiff/Laguerre_test/user_flux.jl
+++
@@ -1,9 +0,0 @@
-function user_flux!(F, G, SD::NSD_2D, q, mesh::St_mesh, ip; neqs=1)
-    #
-    #F(q(x)) = 0.8*q
-    #G(q(x)) = 0.8*q
-    #    
-    F[1] = 0.8*q[1]
-    G[1] = 0.0*q[1]
-    
-end
