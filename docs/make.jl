d470237688514d4edf3ed7165c183e596c30cdae
Simone <smarras@njit.edu>
2023-11-03 11:18:25 -0400

    docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,7 @@
 push!(LOAD_PATH,"../src/")
 
-<<<<<<< HEAD
 using Documenter, Jexpresso
 
-=======
->>>>>>> sm/efficient+laguerre
 makedocs(
     sitename="Jexpresso.jl",
     modules=[Jexpresso],
@@ -12,21 +9,8 @@ makedocs(
     pages = Any[
         "Home" => "index.md",
     ],
-<<<<<<< HEAD
  )
 
 deploydocs(;
            repo="github.com/smarras79/Jexpresso.jl",
            )
-
-=======
-    repo="https://github.com/smarras79/Jexpresso.jl/blob/{commit}{path}#L{line}",
-    authors="S. Marras <smarras@njit.edu>, Y. Tissaoui <yt277@njit.edu>"
-    # warnonly=true, # for debugging
-)
-
-deploydocs(
-    repo="https://github.com/smarras79/Jexpresso.jl/blob/{commit}{path}#L{line}",
-#           repo="github.com/smarras79/Jexpresso.jl",
-)
->>>>>>> sm/efficient+laguerre
