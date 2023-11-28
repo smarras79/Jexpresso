3278304d3a2b277d13917aa5c688540bc7e55808
smarras79 <smarras@njit.edu>
2023-11-28 16:11:38 -0500

    clean up of theta/user_bc.jl from outdated documentation strings.

--- problems/equations/CompEuler/thetaTracers/user_bc.jl
+++ problems/equations/CompEuler/thetaTracers/user_bc.jl
@@ -1,29 +1,3 @@
-"""
-    qibdy is an Array{Floats} of size `nvars`
-
-    src/equations/EQUATIONS_NAME/user_bc.jl contains a set of user-defined boundary conditions functions
-    that can be modified as needed.
-
-    The function defined in src/equations/EQUATIONS_NAME/user_bc.jl 
-    are called by the b.c. functions defined in src/kernel/custom_bcs.jl
-    within a boundary-edge loop that detects the "tag" string defined in the user-generated *.msh file.
-
-    For example:
-    If some domain boundaries of gmsh file mymesh.msh are tagged as "inflow" and "no_slip", then the user
-    creating the functions in user_bc.jl must define the behavior of the unknown or its derivatives
-    on those boundaries.
-
-    ```math
-    if (tag === "inflow")
-        qibdy[1] = 3.0
-    elseif (tag === "fix_temperature")
-        qibdy[2] = 300.0
-    end
-    return qibdy
-    ```
-    where  `qibdy[i=1:nvar]` is the value unknown `i`
-    
-"""
 function user_bc_dirichlet!(q::SubArray{Float64}, x::AbstractFloat, y::AbstractFloat, t::AbstractFloat, tag::String, qbdy::AbstractArray, nx, ny,qe::SubArray{Float64},::TOTAL)
 #    if (tag == "free_slip")
      #if ((x == 5000.0 && y == 0.0) || (x == -5000.0 && y == 0.0) || (x == -5000.0 && y == 10000.0) || (x == 5000.0 && y == 10000.0)) 
