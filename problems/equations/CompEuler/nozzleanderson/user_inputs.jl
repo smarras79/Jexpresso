fb96cda5701324094626ca8028a7eb07a2542728
Simone <smarras@njit.edu>
2023-12-01 18:42:39 -0500

    nozzle user_input.jl tend

--- problems/equations/CompEuler/nozzleanderson/user_inputs.jl
+++ problems/equations/CompEuler/nozzleanderson/user_inputs.jl
@@ -3,7 +3,7 @@ function user_inputs()
         #---------------------------------------------------------------------------
         # User define your inputs below: the order doesn't matter
         #---------------------------------------------------------------------------
-        :tend                 => 0.1,
+        :tend                 => 0.05,
         :ode_solver           => SSPRK33(), #ORK256(),#SSPRK33(), #SSPRK33(), #MSRK5(), #SSPRK54(),
         :Δt                   => 0.00001,
         :ndiagnostics_outputs => 20,
