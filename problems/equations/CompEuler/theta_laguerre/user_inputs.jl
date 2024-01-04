c61eea45275bbd0ceb1a67e636eddcc8f2048534
Yassine <yassinetissaoui@gmail.com>
2024-01-04 10:23:44 -0500

    pushed missing files

---
+++ problems/equations/CompEuler/theta_laguerre/user_inputs.jl
@@ -0,0 +1,64 @@
+function user_inputs()
+    inputs = Dict(
+        #---------------------------------------------------------------------------
+        # User define your inputs below: the order doesn't matter
+        #---------------------------------------------------------------------------
+        :tend                 => 1000.0,
+        :ode_solver           => SSPRK33(), #ORK256(),#SSPRK33(), #SSPRK33(), #MSRK5(), #SSPRK54(),
+        :Δt                   => 0.1,
+        :ndiagnostics_outputs => 20,
+        :case                 => "rtb",
+        :lsource              => true, 
+        #:CL                   => NCL(), #CL() is defaults
+        :SOL_VARS_TYPE        => PERT(), #TOTAL() is default
+        #---------------------------------------------------------------------------
+        #Integration and quadrature properties
+        #---------------------------------------------------------------------------
+        #:lexact_integration  => true,
+        #:llump               => true,
+        :interpolation_nodes =>"lgl",
+        :nop                 => 4,      # Polynomial order
+        :nop_laguerre        => 24,
+        :xfac_laguerre       => 0.0,
+        :yfac_laguerre       => 110.0,
+        :llaguerre_bc        => true,
+        :laguerre_tag        =>"free_slip",
+        #---------------------------------------------------------------------------
+        # Physical parameters/constants:
+        #---------------------------------------------------------------------------
+        :lvisc                => true, #false by default NOTICE: works only for Inexact
+        :ivisc_equations      => (1, 2, 3, 4),
+        :μ                   => (0.0, 60.0, 60.0, 120.0), #horizontal viscosity constant for momentum
+        #---------------------------------------------------------------------------
+        # Mesh paramters and files:
+        #---------------------------------------------------------------------------
+        :lread_gmsh          => true, #If false, a 1D problem will be enforced
+        #:gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_RTB_unitsize.msh",
+        :gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_RTB20x20_Lag.msh", #for nop=4
+        #:gmsh_filename       => "./meshes/gmsh_grids/mygmsh.msh", #for nop=4
+        #:gmsh_filename        => "./meshes/gmsh_grids/test_allocation.msh",
+        #:gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_120x31_periodic.msh",
+        #:gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_RTB27x27.msh", #for nop=3
+        #:gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_RTB40x40.msh", #for nop=2
+        #---------------------------------------------------------------------------
+        # Filter parameters
+        #---------------------------------------------------------------------------
+        :lfilter             => true,
+        :mu_x                => 0.01,
+        :mu_y                => 0.01,
+        :filter_type         => "erf",
+        #---------------------------------------------------------------------------
+        # Plotting parameters
+        #---------------------------------------------------------------------------
+        :outformat           => "vtk",
+        :output_dir          => "./output/",
+        :loutput_pert        => true,  #this is only implemented for VTK for now
+        #---------------------------------------------------------------------------
+    ) #Dict
+    #---------------------------------------------------------------------------
+    # END User define your inputs below: the order doesn't matter
+    #---------------------------------------------------------------------------
+
+    return inputs
+    
+end
