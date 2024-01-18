8b983b2ddeb17ecb8d4caaf8c7db95f52c808d9f
smarras79 <smarras@njit.edu>
2024-01-18 12:00:01 -0500

    push more tests

--- src/kernel/kernelAbstractions/je_rmse.jl
+++ src/kernel/kernelAbstractions/je_rmse.jl
@@ -1,16 +1,51 @@
 using Metal
 using KernelAbstractions
+using BenchmarkTools
 
 @kernel function ka_rmse_kernel(C, A, B)
     i = @index(Global)
 
     a = A[i]
     b = B[i]
-    KernelAbstractions.@atomic C[] += (a-b)^2
+    KernelAbstractions.@atomic C[] += (a-b)^2 #atomic avoids race condition. Notice that @atomic is slow. write the kernel faster without using @atomic.
 end
 
-A = rand(Float32, 512, 512)
-B = rand(Float32, 512, 512)
+@kernel function ka_rmse_kernel_optimal1(C, A, B)
+
+    #This is similar to above, but it uses `stride` to minimize @atomic operations
+    
+    i = (blockIdx().x-1) * blockDim().x + threadIdx().x
+    stride = gridDim().x * blockDim().x
+
+    # grid-stride loop
+    while i <= length(A)
+        a = A[i]
+        b = B[i]
+        KernelAbstractions.@atomic C[] += (a-b)^2
+
+        i += stride
+    end
+end
+
+@kernel function ka_rmse_kernel_optimal2(C, A, B)
+    i = (blockIdx().x-1) * blockDim().x + threadIdx().x
+    stride = gridDim().x * blockDim().x
+
+    # grid-stride loop
+    val = zero(T)
+    while i <= length(A)
+        a = A[i]
+        b = B[i]
+        val += (a-b)^2                 # <-- changed
+
+        i += stride
+    end
+    KernelAbstractions.@atomic C[] += val            # <-- changed
+end
+
+
+A = rand(Float32, 20048, 20048)
+B = rand(Float32, 20048, 20048)
 
 backend = CPU()
 dA = KernelAbstractions.allocate(backend, eltype(A), size(A))
@@ -20,10 +55,10 @@ KernelAbstractions.copyto!(backend, dB, B)
 dC = KernelAbstractions.zeros(backend, eltype(A), 1)
 @show typeof(dC)
 
-k = ka_rmse_kernel(backend)
-k(dC, dA, dB; ndrange=size(A))
+@btime ka_rmse_kernel(backend)
 
-sqrt(Array(dC)[] / length(A))
+#k(dC, dA, dB; ndrange=size(A))
+#sqrt(Array(dC)[] / length(A))
      
 
 backend = MetalBackend()
@@ -32,9 +67,12 @@ KernelAbstractions.copyto!(backend, dA, A)
 dB = KernelAbstractions.allocate(backend, eltype(B), size(B))
 KernelAbstractions.copyto!(backend, dB, B)
 dC = KernelAbstractions.zeros(backend, eltype(A), 1)
-@show typeof(dC)
 
-k = ka_rmse_kernel(backend)
-k(dC, dA, dB; ndrange=size(A))
+@benchmark ka_rmse_kernel(backend)
 
-sqrt(Array(dC)[] / length(A))
+@benchmark ka_rmse_kernel_optimal1(backend)
+
+@benchmark ka_rmse_kernel_optimal2(backend)
+
+#k(dC, dA, dB; ndrange=size(A))
+#sqrt(Array(dC)[] / length(A))
