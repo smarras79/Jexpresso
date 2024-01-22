310ae360d5f0e4db9d0148bf5296e727917b562b
smarras79 <smarras@njit.edu>
2024-01-22 14:07:21 -0500

    working in Gaussian rotation in AdvDiff/circle with: diffusion on nothing in user_bc and *.msh

--- problems/equations/AdvDiff/circle/user_inputs.jl
+++ problems/equations/AdvDiff/circle/user_inputs.jl
@@ -3,7 +3,7 @@ function user_inputs()
         #---------------------------------------------------------------------------
         # User define your inputs below: the order doesn't matter
         #---------------------------------------------------------------------------
-        :tend                 => 3.14,
+        :tend                 => 6.28,
         :ode_solver           => SSPRK53(), #ORK256(),#SSPRK33(), #SSPRK33(), #MSRK5(), #SSPRK54(),
         :Δt                   => 0.001,
         :ndiagnostics_outputs => 10,
@@ -18,13 +18,13 @@ function user_inputs()
         #---------------------------------------------------------------------------
         :lvisc                => true, #false by default NOTICE: works only for Inexact
         :ivisc_equations      => (1),
-        :μ                    => (0.01), #kinematic viscosity constant for θ equation
+        :μ                    => (0.0025), #kinematic viscosity constant for θ equation
         #---------------------------------------------------------------------------
         # Mesh paramters and files:
         #---------------------------------------------------------------------------
         :lread_gmsh          => true, #If false, a 1D problem will be enforced
         #:gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_10x10_periodic_transport.msh",
-        :gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_10x10.msh",
+        :gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_10x10.msh", #this is with NOTHING in user_bc
         #:gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_10x10_free_slip.msh",
         #---------------------------------------------------------------------------
         # Plotting parameters
