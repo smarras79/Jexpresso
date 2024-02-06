c2ea0287880b1919ba4e86ccfbfcd87117cd8adf
smarras79 <smarras@njit.edu>
2024-02-06 13:21:37 -0500

    Added wave1d-fd

--- problems/equations/CompEuler/wave1d-fd/user_inputs.jl
+++ problems/equations/CompEuler/wave1d-fd/user_inputs.jl
@@ -8,20 +8,21 @@ function user_inputs()
         :ode_solver          => SSPRK33(),
         :tend                 => 3.0,
         :Δt                   => 1.0e-3,
-        :ndiagnostics_outputs => 30, #these are steps, not seconds
+        :ndiagnostics_outputs => 10, #these are steps, not seconds
         :output_dir          => "./",
         #---------------------------------------------------------------------------
         #Integration and quadrature properties
         #---------------------------------------------------------------------------
+        :AD                  => FD(),
         :interpolation_nodes => "lgl", # Choice: "lgl", "cg", "cgl"
-        :nop                 => 6,     # Polynomial order
+        :nop                 => 1,     # Polynomial order
         :lexact_integration  => false,
         :lsource             => false,
         :lperiodic_1d        => true, #false by default
         #---------------------------------------------------------------------------
         # Physical parameters/constants:
         #---------------------------------------------------------------------------
-        :lvisc                => false,
+        :lvisc                => false, #false by default NOTICE: works only for Inexact
         :ivisc_equations      => 1,
         :μ                   => 0.1, #horizontal viscosity constant for momentumx
         #---------------------------------------------------------------------------
