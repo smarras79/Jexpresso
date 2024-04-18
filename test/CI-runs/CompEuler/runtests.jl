5d3eccac9e3d74ae7384cbc0a454429ab2753a3d
smarras79 <smarras@njit.edu>
2024-04-18 14:08:39 -0400

    added thetaTracers to CI tests

--- test/CI-runs/CompEuler/runtests.jl
+++ test/CI-runs/CompEuler/runtests.jl
@@ -3,5 +3,6 @@ module CompEulerTests
 using Test
 
 @testset "theta" begin include("theta/thetaTests.jl") end
+@testset "thetaTracers" begin include("thetaTracers/thetaTests.jl") end
 
 end # module
