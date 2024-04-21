25c9caa5a7e82237be99ff662bb171000b3647d3
smarras79 <smarras@njit.edu>
2024-04-21 11:28:00 -0400

    WIP: Added some more to Documentation. push to test deployment.

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,25 @@
 push!(LOAD_PATH,"../src/")
 
-using Documenter
-#using Jexpresso
+using Documenter, PrettyTables, Latexify #, Jexpresso
 
-pages = [
-  "Home" => "index.md",
-]
+cp("./docs/Manifest.toml", "./docs/src/assets/Manifest.toml", force = true)
+cp("./docs/Project.toml", "./docs/src/assets/Project.toml", force = true)
+
+include("pages.jl")
 
 makedocs(
-  sitename = "Jexpresso.jl",
-  format = Documenter.HTML(
-    size_threshold=nothing
-  ),
-  modules = [Jexpresso],
-  pages = pages,
-  doctest = false,
-  warnonly = [:cross_references,:missing_docs],
-  checkdocs = :exports,
-)
+    sitename = "Jexpresso.jl",
+    format = Documenter.HTML(assets = ["assets/favicon.ico"],
+                             canonical = "https://smarras79.github.io/Jexpresso/dev/",
+                             size_threshold=nothing
+                             ),
+    authors = "Simone Marras, Yassine Tissaoui, Hang Wang",
+    modules = [Jexpresso],
+    #linkcheck = true,
+    doctest = false, clean = true,
+    pages = pages,
+    warnonly = [:cross_references,:missing_docs],
+    checkdocs = :exports,
+    )
 
-deploydocs(;
-           repo="github.com/smarras79/Jexpresso.jl",
-)
+deploydocs(repo="github.com/smarras79/Jexpresso.jl")
