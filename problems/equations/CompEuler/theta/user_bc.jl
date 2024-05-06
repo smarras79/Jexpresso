54f8bcc0688244b4f4c03c5cd6cf1efdc135c76b
smarras79 <smarras@njit.edu>
2024-05-06 08:29:53 -0400

    WIP: new data structures for allocation

--- problems/equations/CompEuler/theta/user_bc.jl
+++ problems/equations/CompEuler/theta/user_bc.jl
@@ -1,26 +1,26 @@
 function user_bc_dirichlet!(q::SubArray{TFloat}, x::AbstractFloat, y::AbstractFloat, t::AbstractFloat, tag::String, qbdy::AbstractArray, nx, ny,qe::SubArray{TFloat},::TOTAL)
-#    if (tag == "free_slip")
-     #if ((x == 5000.0 && y == 0.0) || (x == -5000.0 && y == 0.0) || (x == -5000.0 && y == 10000.0) || (x == 5000.0 && y == 10000.0)) 
-     #  a = 1
-     #  b = 1
-     #  if (x > 0)
-     #    a= -1
-     #  end
-     #  if (y > 0)
-     #    b = -1
-     #  end
-     #  qnl = a*(sqrt(2)/2)*q[2] + b*(sqrt(2)/2)*q[3]
-      
+    #    if (tag == "free_slip")
+    #if ((x == 5000.0 && y == 0.0) || (x == -5000.0 && y == 0.0) || (x == -5000.0 && y == 10000.0) || (x == 5000.0 && y == 10000.0)) 
+    #  a = 1
+    #  b = 1
+    #  if (x > 0)
+    #    a= -1
+    #  end
+    #  if (y > 0)
+    #    b = -1
+    #  end
+    #  qnl = a*(sqrt(2)/2)*q[2] + b*(sqrt(2)/2)*q[3]
+    
     # else
-       qnl = nx*q[2] + ny*q[3]
+    qnl = nx*q[2] + ny*q[3]
     # end
-     qbdy[2] = q[2] - qnl*nx
-     qbdy[3] = q[3] - qnl*ny
+    qbdy[2] = q[2] - qnl*nx
+    qbdy[3] = q[3] - qnl*ny
 
- #   else
-  #    qbdy[2] = 0.0
-   # end
-   #return qbdy #, flags
+    #   else
+    #    qbdy[2] = 0.0
+    # end
+    #return qbdy #, flags
     
 end
 
