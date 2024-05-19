569c5727305a614bb66e0cebdb46db40b7dfd04c
smarras79 <smarras@njit.edu>
2024-05-19 11:43:26 -0400

    1. Added theta_laguerre to CI 2. re-run theta, thetaTracers, wave1d_lag for CI after last changes from sm/gpu

--- test/CI-runs/CompEuler/theta_laguerre/user_inputs.jl
+++ test/CI-runs/CompEuler/theta_laguerre/user_inputs.jl
@@ -1,53 +1,52 @@
 function user_inputs()
-    
     inputs = Dict(
         #---------------------------------------------------------------------------
         # User define your inputs below: the order doesn't matter
         #---------------------------------------------------------------------------
-        :ode_solver           => SSPRK54(), #ORK256(),#SSPRK33(), #SSPRK33(), #SSPRK54(),
-        :Δt                   => 0.4,
+        :tend                 => 1000.0,
+        :ode_solver           => SSPRK33(), #ORK256(),#SSPRK33(), #SSPRK33(), #MSRK5(), #SSPRK54(),
         :tinit                => 0.0,
-        :tend                 => 1000,
-        #:tinit                => 100.0,
-        #:tend                 => 1000.0,
-        #:lrestart             => true,
-        :restart_input_file_path => "./output/CompEuler/theta/output-19Nov2023-115126",
+        :Δt                   => 0.05,
         :ndiagnostics_outputs => 2,
         :case                 => "rtb",
         :lsource              => true, 
-        #:backend              => MetalBackend(),
-        #:SOL_VARS_TYPE        => PERT(), #TOTAL() is default
+        :SOL_VARS_TYPE        => PERT(), #TOTAL() is default
         #---------------------------------------------------------------------------
         #Integration and quadrature properties
         #---------------------------------------------------------------------------
+        #:lexact_integration  => true,
+        #:llump               => true,
         :interpolation_nodes =>"lgl",
         :nop                 => 4,      # Polynomial order
+        :nop_laguerre        => 16,
+        :xfac_laguerre       => 0.0,
+        :yfac_laguerre       => 110.0,
+        :llaguerre_bc        => true,
+        :laguerre_tag        =>"free_slip",
         #---------------------------------------------------------------------------
         # Physical parameters/constants:
         #---------------------------------------------------------------------------
         :lvisc                => true, #false by default NOTICE: works only for Inexact
         :ivisc_equations      => [1, 2, 3, 4],
-        :μ                   => [0.0, 20.0, 20.0, 60.0], #horizontal viscosity constant for momentum
+        :μ                   => [0.0, 125.0, 125.0, 125.0], #horizontal viscosity constant for momentum
         #---------------------------------------------------------------------------
         # Mesh paramters and files:
         #---------------------------------------------------------------------------
         :lread_gmsh          => true, #If false, a 1D problem will be enforced
-        #:gmsh_filename       => "./meshes/gmsh_grids/square_UNSTR_20el.msh", #for nop=4
-        :gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_RTB20x20.msh", #for nop=4
+        :gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_RTB20x20_Lag.msh", #for nop=4
         #---------------------------------------------------------------------------
         # Filter parameters
         #---------------------------------------------------------------------------
         #:lfilter             => true,
-        #:mu_x                => 0.01,
-        #:mu_y                => 0.01,
-        #:filter_type         => "erf",
+        :mu_x                => 0.01,
+        :mu_y                => 0.01,
+        :filter_type         => "erf",
         #---------------------------------------------------------------------------
         # Plotting parameters
         #---------------------------------------------------------------------------
-        :outformat           => "hdf5",
         :loverwrite_output   => true,
-        :lwrite_initial      => false,
-        :output_dir          => "./test/CI-ref", #"./output/",
+        :outformat           => "hdf5",
+        :output_dir          => "./test/CI-ref",
         :loutput_pert        => true,  #this is only implemented for VTK for now
         #---------------------------------------------------------------------------
     ) #Dict
