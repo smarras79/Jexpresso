36d267f2e4ce39e312801c407390f0f37ec85cf8
smarras79 <smarras@njit.edu>
2024-05-20 07:50:04 -0400

    added missing CI-run/.../.../output dirrectory to repo

---
+++ test/CI-runs/CompEuler/theta/output/user_inputs.jl
@@ -0,0 +1,56 @@
+function user_inputs()
+    inputs = Dict(
+        #---------------------------------------------------------------------------
+        # User define your inputs below: the order doesn't matter
+        #---------------------------------------------------------------------------
+        :ode_solver           => SSPRK54(), #ORK256(),#SSPRK33(), #SSPRK33(), #SSPRK54(),
+        :Δt                   => 0.4,
+        :tinit                => 0.0,
+        :tend                 => 1000,
+        #:tinit                => 100.0,
+        #:tend                 => 1000.0,
+        #:lrestart             => true,
+        #:restart_input_file_path => "./output/CompEuler/theta/output-19Nov2023-115126",
+        :ndiagnostics_outputs => 2,
+        :case                 => "rtb",
+        :lsource              => true, 
+        :SOL_VARS_TYPE        => TOTAL(), #TOTAL() is default
+        #---------------------------------------------------------------------------
+        #Integration and quadrature properties
+        #---------------------------------------------------------------------------
+        :interpolation_nodes =>"lgl",
+        :nop                 => 4,      # Polynomial order
+        #---------------------------------------------------------------------------
+        # Physical parameters/constants:
+        #---------------------------------------------------------------------------
+        :lvisc                => true, #false by default NOTICE: works only for Inexact
+        :ivisc_equations      => (1, 2, 3, 4),
+        :μ                   => (0.0, 20.0, 20.0, 20.0), #horizontal viscosity constant for momentum
+        #---------------------------------------------------------------------------
+        # Mesh paramters and files:
+        #---------------------------------------------------------------------------
+        :lread_gmsh          => true, #If false, a 1D problem will be enforced
+        :gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_RTB20x20.msh", #for nop=4
+        #---------------------------------------------------------------------------
+        # Filter parameters
+        #---------------------------------------------------------------------------
+        #:lfilter             => true,
+        #:mu_x                => 0.01,
+        #:mu_y                => 0.01,
+        #:filter_type         => "erf",
+        #---------------------------------------------------------------------------
+        # Plotting parameters
+        #---------------------------------------------------------------------------
+        :outformat           => "hdf5",
+        :loverwrite_output   => true,
+        :output_dir          => "./test/CI-runs", #this is in github
+        :loutput_pert        => true,  #this is only implemented for VTK for now
+        #---------------------------------------------------------------------------
+    ) #Dict
+    #---------------------------------------------------------------------------
+    # END User define your inputs below: the order doesn't matter
+    #---------------------------------------------------------------------------
+
+    return inputs
+    
+end
