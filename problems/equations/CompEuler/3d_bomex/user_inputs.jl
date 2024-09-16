ec6e9d966e36a82986d739877a2e2c2a4ae3706d
Simone <smarras@njit.edu>
2024-09-16 18:31:51 -0400

    added a 32x32x38.msh to user_inputs.jl for bomex

--- problems/equations/CompEuler/3d_bomex/user_inputs.jl
+++ problems/equations/CompEuler/3d_bomex/user_inputs.jl
@@ -35,7 +35,8 @@ function user_inputs()
         #---------------------------------------------------------------------------
         :lread_gmsh           => true, #If false, a 1D problem will be enforced
         # :gmsh_filename        => "./meshes/gmsh_grids/hexa_BOMEX-5x1x19.msh",
-        :gmsh_filename       => "./meshes/gmsh_grids/hexa_BOMEX-16x16x19.msh",
+        #:gmsh_filename       => "./meshes/gmsh_grids/hexa_BOMEX-16x16x19.msh",
+        :gmsh_filename       => "./meshes/gmsh_grids/hexa_BOMEX-32x32x38.msh",
         #:gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_10x1x10.msh",
         #---------------------------------------------------------------------------
         # Filter parameters
