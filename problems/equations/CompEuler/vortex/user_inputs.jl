ad02c295cdaef76fd8e0dc822ee66b9275f1c0a9
smarras79 <smarras@njit.edu>
2024-10-16 13:37:02 -0400

    vortex and periodic-euler

---
+++ problems/equations/CompEuler/vortex/user_inputs.jl
@@ -0,0 +1,57 @@
+function user_inputs()
+    
+    inputs = Dict(
+        #---------------------------------------------------------------------------
+        # User define your inputs below: the order doesn't matter
+        #---------------------------------------------------------------------------
+        :ode_solver           => SSPRK54(), #ORK256(),#SSPRK33(), #SSPRK33(), #SSPRK54(),
+        :Δt                   => 0.005,
+        :tinit                => 0.0,
+        :tend                 => 20.0,
+        :diagnostics_at_times => (1, 2, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 12, 14, 16, 18, 20), 
+        :case                 => "vortex",
+        :lsource              => true,
+        :lperiodic            => true,
+        #:backend              => MetalBackend(),
+        #:SOL_VARS_TYPE        => PERT(), #TOTAL() is default
+        #---------------------------------------------------------------------------
+        #Integration and quadrature properties
+        #---------------------------------------------------------------------------
+        :interpolation_nodes =>"lgl",
+        :nop                 => 4,      # Polynomial order
+        #---------------------------------------------------------------------------
+        # Physical parameters/constants:
+        #---------------------------------------------------------------------------
+        :lvisc                => true, #false by default NOTICE: works only for Inexact
+        :ivisc_equations      => (1, 2, 3, 4),
+        :μ                    => (0.0, 0.01, 0.01, 0.01), #horizontal viscosity constant for momentum
+        #---------------------------------------------------------------------------
+        # Mesh paramters and files:
+        #---------------------------------------------------------------------------
+        :lread_gmsh          => true, #If false, a 1D problem will be enforced
+        :gmsh_filename       => "./meshes/gmsh_grids/unit_square_20x20.msh", #for nop=4
+        #---------------------------------------------------------------------------
+        # Filter parameters
+        #---------------------------------------------------------------------------
+        #:lfilter             => true,
+        #:mu_x                => 0.01,
+        #:mu_y                => 0.01,
+        #:filter_type         => "erf",
+        #---------------------------------------------------------------------------
+        # Plotting parameters
+        #---------------------------------------------------------------------------
+        :outformat           => "png",
+        :loverwrite_output   => true,
+        :lwrite_initial      => true,
+        :output_dir          => "./output",
+        #:output_dir          => "./test/CI-run",
+        :loutput_pert        => true,  #this is only implemented for VTK for now
+        #---------------------------------------------------------------------------
+    ) #Dict
+    #---------------------------------------------------------------------------
+    # END User define your inputs below: the order doesn't matter
+    #---------------------------------------------------------------------------
+
+    return inputs
+    
+end
