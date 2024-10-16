7f36706740f42732b1c9996bbf7a9e22456a8dc7
Simone <smarras@njit.edu>
2024-10-16 15:10:34 -0400

    docs make missing comma fix. vortex added 3 cases from https://ntrs.nasa.gov/api/citations/20150018403/downloads/20150018403.pdf#page=18.49

--- problems/equations/CompEuler/vortex/user_flux.jl
+++ problems/equations/CompEuler/vortex/user_flux.jl
@@ -15,16 +15,18 @@ function user_flux!(F::SubArray{TFloat}, G::SubArray{TFloat}, SD::NSD_2D,
     v = ρv/ρ
     
     T = (ρe/ρ - 0.5*sqrt(u*u + v*v))/PhysConst.cv
-    
+
+    gm1 = PhysConst.γ - 1.0
+    #Pressure = gm1*(ρe - 0.5*ρ*(u*u + v*v))
     Pressure = perfectGasLaw_ρTtoP(PhysConst, ρ=ρ, Temp=T)
     
     F[1] = ρu
     F[2] = ρu*u .+ Pressure
     F[3] = ρv*u
-    F[4] = ρe*u .+ Pressure*u 
+    F[4] = u*(ρe .+ Pressure)
 
     G[1] = ρv
     G[2] = ρu*v
     G[3] = ρv*v .+ Pressure
-    G[4] = ρe*v .+ Pressure*v
+    G[4] = v*(ρe .+ Pressure)
 end
--- problems/equations/CompEuler/vortex/user_inputs.jl
+++ problems/equations/CompEuler/vortex/user_inputs.jl
@@ -9,9 +9,14 @@ function user_inputs()
         :tinit                => 0.0,
         :tend                 => 20.0,
         :diagnostics_at_times => (1, 2, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 12, 14, 16, 18, 20), 
-        :case                 => "vortex",
+
+        :case                 => "shu",
+        #:case                 => "hw",
+        #:case                 => "vincent",
+
         :lsource              => true,
         :lperiodic            => true,
+
         #:backend              => MetalBackend(),
         #:SOL_VARS_TYPE        => PERT(), #TOTAL() is default
         #---------------------------------------------------------------------------
@@ -23,13 +28,13 @@ function user_inputs()
         # Physical parameters/constants:
         #---------------------------------------------------------------------------
         :lvisc                => true, #false by default NOTICE: works only for Inexact
-        :ivisc_equations      => (1, 2, 3, 4),
-        :μ                    => (0.0, 0.01, 0.01, 0.01), #horizontal viscosity constant for momentum
+        :ivisc_equations      => [1, 2, 3, 4],
+        :μ                    => [0.0, 0.01, 0.01, 0.01], #horizontal viscosity constant for momentum
         #---------------------------------------------------------------------------
         # Mesh paramters and files:
         #---------------------------------------------------------------------------
         :lread_gmsh          => true, #If false, a 1D problem will be enforced
-        :gmsh_filename       => "./meshes/gmsh_grids/unit_square_20x20.msh", #for nop=4
+        :gmsh_filename       => "./meshes/gmsh_grids/vortex_20x20.msh", #for nop=4
         #---------------------------------------------------------------------------
         # Filter parameters
         #---------------------------------------------------------------------------
@@ -45,7 +50,7 @@ function user_inputs()
         :lwrite_initial      => true,
         :output_dir          => "./output",
         #:output_dir          => "./test/CI-run",
-        :loutput_pert        => true,  #this is only implemented for VTK for now
+        #:loutput_pert        => true,  #this is only implemented for VTK for now
         #---------------------------------------------------------------------------
     ) #Dict
     #---------------------------------------------------------------------------
