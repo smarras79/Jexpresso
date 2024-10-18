810686f719eacbbc6a8db71577c7632eab8058fd
Simone <smarras@njit.edu>
2024-10-18 08:15:55 -0400

    added problems/equations/Elliptic/case1

---
+++ problems/equations/Elliptic/case1/user_source.jl
@@ -0,0 +1,30 @@
+function user_source(S,
+                      q, 
+                      qe,
+                      npoin::Int64,
+                      ::CL, ::TOTAL;
+                      neqs=1,x=0.0, y=0.0, ymin=0.0, ymax=30000.0, xmin = -120000, xmax =120000)
+
+    PhysConst = PhysicalConst{Float64}()
+        
+    #
+    # S(q(x)) = -ρg
+    #
+    L = 2
+    alpha = 10
+    f = - (cos(x/L) * exp(-x/L)*cos(y))/L - sin(x/L)*exp(-x/L)*cos(y)
+    u_e = sin(x/L)*exp(-x/L)*cos(y)
+        
+    S = f - alpha*u_e
+    return S
+end
+
+function user_source_gpu(q, qe, x, y)
+    T = eltype(q)
+    L = 2
+    alpha = 10 
+    f = T(- (cos(x/L) * exp(-x/L)*cos(y))/L - sin(x/L)*exp(-x/L)*cos(y))
+    u_e = T(sin(x/L)*exp(-x/L)*cos(y))
+
+    return T(f - alpha*u_e)
+end
