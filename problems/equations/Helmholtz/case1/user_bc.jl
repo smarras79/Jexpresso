52d94c5181c3aba9cb04a0c370a452ceab0680d1
Simone <smarras@njit.edu>
2024-10-22 14:17:32 -0400

    WIP: fixing BC for elliptic

--- problems/equations/Helmholtz/case1/user_bc.jl
+++ problems/equations/Helmholtz/case1/user_bc.jl
@@ -25,31 +25,11 @@
     
 """
 function user_bc_dirichlet!(q::SubArray{Float64}, x::AbstractFloat, y::AbstractFloat, t::AbstractFloat, tag::String, qbdy::AbstractArray, nx, ny,qe::SubArray{Float64},::TOTAL)
-    if (tag == "free_slip")
     
-        qnl = nx*q[2] + ny*q[3]
-        qbdy[2] = q[2] - qnl*nx
-        qbdy[3] = q[3] - qnl*ny
+    if (lowercase(tag) == "laguerre")
+        qbdy[1] = 0.0
+    else
+        qbdy[1] = 0.0
     end
     
-end
-
-function user_bc_dirichlet!(q::SubArray{Float64}, x::AbstractFloat, y::AbstractFloat, t::AbstractFloat, tag::String, qbdy::AbstractArray, nx::AbstractFloat, ny::AbstractFloat,qe::SubArray{Float64},::PERT)
-     #if (y <4995 || abs(x) > 4995)
-     if (tag == "free_slip")
-        qnl = nx*(q[2]+qe[2]) + ny*(q[3]+qe[3])
-        qbdy[2] = (q[2]+qe[2] - qnl*nx) - qe[2]
-        qbdy[3] = (q[3]+qe[3] - qnl*ny) - qe[3]
-     end
-
-end
-
-function user_bc_neumann(q::AbstractArray, gradq::AbstractArray, x::AbstractFloat, y::AbstractFloat, t::AbstractFloat, tag::String, inputs::Dict)
-    flux = zeros(size(q,2),1)
-    return flux
-end
-
-function user_bc_neumann(q::AbstractArray, gradq::AbstractArray, x::AbstractFloat, t::AbstractFloat, inputs::Dict)
-    flux = zeros(size(q,2),1)
-    return flux
 end
