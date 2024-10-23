78ea3e7f9beebcfce7ab0182c8450d08c72ed6f4
Yassine <yassinetissaoui@gmail.com>
2024-10-23 09:05:37 -0500

    pushing 3d periodic test case

---
+++ problems/equations/AdvDiff/3d_periodic/user_inputs.jl
@@ -0,0 +1,59 @@
+function user_inputs()
+    inputs = Dict(
+        #---------------------------------------------------------------------------
+        # User define your inputs below: the order doesn't matter
+        #---------------------------------------------------------------------------
+        :ode_solver           => SSPRK54(), #ORK256(),#SSPRK33(), #SSPRK33(), #SSPRK54(),
+        :Δt                   => 0.001,
+        :tinit                => 0.0,
+        :tend                 => 10.0,
+        #:tinit                => 100.0,
+        #:tend                 => 1000.0,
+        #:lrestart             => true,
+        :restart_input_file_path => "./output/CompEuler/theta/output-19Nov2023-115126",
+        :ndiagnostics_outputs => 10,
+        :case                 => "rtb",
+        :lsource              => true, 
+        #:backend              => MetalBackend(),
+        #---------------------------------------------------------------------------
+        #Integration and quadrature properties
+        #---------------------------------------------------------------------------
+        :interpolation_nodes =>"lgl",
+        :nop                 => 5,      # Polynomial order
+        #---------------------------------------------------------------------------
+        # Physical parameters/constants:
+        #---------------------------------------------------------------------------
+        #:lvisc                => true, #false by default NOTICE: works only for Inexact
+        :ivisc_equations      => [1, 2, 3, 4, 5],
+        :μ                   => [0.0, 20.0, 20.0, 20.0, 60.0], #horizontal viscosity constant for momentum
+        #---------------------------------------------------------------------------
+        # Mesh paramters and files:
+        #---------------------------------------------------------------------------
+        :lread_gmsh          => true, #If false, a 1D problem will be enforced
+        #:gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_2x1x1.msh",
+        #:gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_10x1x10.msh",
+        :gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_periodic3D.msh",
+        #:gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_20x1x20.msh",
+        #---------------------------------------------------------------------------
+        # Filter parameters
+        #---------------------------------------------------------------------------
+        #:lfilter             => true,
+        #:mu_x                => 0.01,
+        #:mu_y                => 0.01,
+        #:filter_type         => "erf",
+        #---------------------------------------------------------------------------
+        # Plotting parameters
+        #---------------------------------------------------------------------------
+        :outformat           => "vtk", #"hdf5",
+        :output_dir          => "./output/",
+        :loutput_pert        => true,  #this is only implemented for VTK for now
+        #:lvolume3d           => true,
+        #---------------------------------------------------------------------------
+    ) #Dict
+    #---------------------------------------------------------------------------
+    # END User define your inputs below: the order doesn't matter
+    #---------------------------------------------------------------------------
+
+    return inputs
+    
+end
