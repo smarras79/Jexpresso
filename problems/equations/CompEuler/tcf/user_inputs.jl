b473c0e2ace44eb8ea96b6ec9fb1f47007037976
Simone <smarras@njit.edu>
2024-10-26 12:20:52 -0400

    broken write_out with periodic with euler

--- problems/equations/CompEuler/tcf/user_inputs.jl
+++ problems/equations/CompEuler/tcf/user_inputs.jl
@@ -31,7 +31,7 @@ function user_inputs()
         # Mesh paramters and files:
         #---------------------------------------------------------------------------
         :lread_gmsh          => true, #If false, a 1D problem will be enforced
-        :gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_periodic3D.msh",
+        :gmsh_filename       => "./meshes/gmsh_grids/hexa_BOMEX-10x10x12.msh",
         #---------------------------------------------------------------------------
         # Filter parameters
         #---------------------------------------------------------------------------
