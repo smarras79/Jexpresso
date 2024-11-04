108e7b6280112830a2bd2ebeb20539ab707a6910
Yassine <yassinetissaoui@gmail.com>
2024-11-04 13:29:27 -0600

    wip gpu ice

--- src/kernel/physics/microphysics.jl
+++ src/kernel/physics/microphysics.jl
@@ -16,7 +16,6 @@ function do_micro_physics!(mp::St_SamMicrophysics, npoin, uaux, z, qe, ::TOTAL)
         mp.qr[ip] = qr
         mp.qs[ip] = qs
         mp.qg[ip] = qg
-        uaux[ip,5] = uaux[ip,1]*(PhysConst.cp*T + PhysConst.g*z[ip] - MicroConst.Lc*(qc + qr) - MicroConst.Ls*(qi + qs + qg))
         uaux[ip,end] = P
 
         ### find Pr, Ps, Pg
@@ -34,6 +33,32 @@ function do_micro_physics!(mp::St_SamMicrophysics, npoin, uaux, z, qe, ::TOTAL)
     ### find vertical derivatives for sedimentation processes. 
     
     return nothing
+end
+
+@kernel function do_micro_physics_gpu_3D!(uaux, qe, Tabs, qn, qi, qc, qr, qs, qg, Pr, Ps, Pg, S_micro, PhysConst, MicroConst, lpert, neq, npoin, z)
+    ie = @index(Group, Linear)
+    i = @index(Local, Linear)
+    ip = connijk[ie,i,1]
+
+    T = eltype(uaux)
+    uip = @view(uaux[ip,1:neq])
+    qeip = @view(qe[ip,1:neq+1])
+
+    Tip, Pip, qnip, qiip, qcip, qrip, qsip, qgip, qsatt = saturation_adjustment_sam_microphysics_gpu!(uip,qeip,
+                                                                                                        z[ip],MicroConst,PhysConst,lpert)
+    KernelAbstractions.@atomic Tabs[ip] = Tip
+    KernelAbstractions.@atomic uaux[ip,end] = Pip
+    KernelAbstractions.@atomic qn[ip] = qnip
+    KernelAbstractions.@atomic qi[ip] = qiip
+    KernelAbstractions.@atomic qc[ip] = qcip
+    KernelAbstractions.@atomic qr[ip] = qrip
+    KernelAbstractions.@atomic qs[ip] = qsip
+    KernelAbstractions.@atomic qg[ip] = qgip
+
+    #KernelAbstractions.@atomic Pr[ip], Ps[ip], Pg[ip] = compute_Pm_gpu(uip, qeip, qr[ip], qs[ip], qg[ip], MicroConst,lpert)
+    
+    #KernelAbstraction.@atomic S_micro[ip] = compute_dqpdt_sam_micro_gpu(uip, qeip, Tabs[ip], uaux[ip,end], qr[ip], qs[ip], qg[ip], qsatt, MicroConst, PhysConst, lpert) 
+
 end
 
 function do_micro_physics!(mp::St_SamMicrophysics, npoin, uaux, z, qe, ::PERT)
@@ -58,16 +83,6 @@ function do_micro_physics!(mp::St_SamMicrophysics, npoin, uaux, z, qe, ::PERT)
         mp.qr[ip] = qr
         mp.qs[ip] = qs
         mp.qg[ip] = qg
-        #hl = (uaux[ip,1]+qe[ip,1])*(PhysConst.cp*T + PhysConst.g*z[ip] - MicroConst.Lc*(qc + qr) - MicroConst.Ls*(qi+qs+qg))
-        #uaux[ip,5] = hl - qe[ip,5]
-        ## retrieve absolute temp
-        #T = (hl/(uaux[ip,1]+qe[ip,1]) - PhysConst.g*z[ip] + MicroConst.Lc*(qc + qr) + MicroConst.Ls*(qi + qs + qg))/PhysConst.cp 
-        #mp.Tabs[ip] = T
-        #P = moistPressure(PhysConst; ρ = uaux[ip,1]+qe[ip,1], Temp = T, qv = (uaux[ip,6]+qe[ip,6])/(uaux[ip,1]+qe[ip,1])-qn)
-        #hl = (uaux[ip,5]+qe[ip,5])/(uaux[ip,1]+qe[ip,1])
-        #T = (hl - PhysConst.g*z[ip] + MicroConst.Lc*(qc + qr) + MicroConst.Ls*(qi+qs+qg))/PhysConst.cp
-        #P = moistPressure(PhysConst; ρ = uaux[ip,1]+qe[ip,1], Temp = T, qv = (uaux[ip,6]+qe[ip,6])/(uaux[ip,1]+qe[ip,1])-qn)
-        #mp.Tabs[ip] = T
         uaux[ip,end] = P
 
         ### find Pr, Ps, Pg
@@ -339,6 +354,115 @@ function compute_dqpdt_sam_micro(ρ,T,P,hl,qt,qn,qc,qi,qp,qr,qs,qg,qsatt,MicroCo
     return dqpdt 
 end
 
+function compute_dqpdt_sam_micro_gpu(u,qe,T,P,qt,qn,qc,qi,qp,qr,qs,qg,qsatt,MicroConst,PhysConst,lpert)
+    FT = eltype(u)
+    if (lpert)
+        ρ = u[1] + qe[1]
+    else
+        ρ = u[1]
+    end
+
+    a_rain = MicroConst.a_rain #Constant in fall speed for rain
+    b_rain = MicroConst.b_rain
+    N0_rain = MicroConst.N0_rain
+    Er_c = MicroConst.Er_c
+    Er_i = MicroConst.Er_i
+    γ3br = MicroConst.γ3br
+    ρ_rain = MicroConst.ρ_rain
+    C_rain = MicroConst.C_rain
+    a_fr   = MicroConst.a_fr
+    b_fr   = MicroConst.b_fr
+    γ5br   = MicroConst.γ5br
+
+    a_snow = MicroConst.a_snow #Constant in fall speed for rain
+    b_snow = MicroConst.b_snow
+    N0_snow = MicroConst.N0_snow
+    Es_c = MicroConst.Es_c
+    Es_i = MicroConst.Es_i
+    γ3bs = MicroConst.γ3bs
+    ρ_snow = MicroConst.ρ_snow
+    C_snow = MicroConst.C_snow
+    a_fs   = MicroConst.a_fs
+    b_fs   = MicroConst.b_fs
+    γ5bs   = MicroConst.γ5bs
+
+    a_graupel = MicroConst.a_graupel #Constant in fall speed for rain
+    b_graupel = MicroConst.b_graupel
+    N0_graupel = MicroConst.N0_graupel
+    Eg_c = MicroConst.Eg_c
+    Eg_i = MicroConst.Eg_i
+    γ3bg = MicroConst.γ3bg
+    ρ_graupel = MicroConst.ρ_graupel
+    C_graupel = MicroConst.C_graupel
+    a_fg   = MicroConst.a_fg
+    b_fg   = MicroConst.b_fg
+    γ5bg   = MicroConst.γ5bg
+
+    ρ0 = MicroConst.ρ0
+    α = MicroConst.α
+    qc0 = MicroConst.qc0
+    β = MicroConst.β
+    qi0 = MicroConst.qi0
+
+    Lc = MicroConst.Lc
+    Ls = MicroConst.Ls
+    Ka = MicroConst.Ka
+    Da = MicroConst.Da
+    Rvap = PhysConst.Rvap
+    Rair = PhysConst.Rair
+    μ = MicroConst.μ
+    e_satw = esatw(T)
+    e_sati = esati(T)
+
+    ### Collection of condensates
+    Ar_c = FT(FT(π)/FT(4))*a_rain*N0_rain*Er_c*γ3br*(ρ0/ρ)^(FT(0.5))*FT((ρ/(FT(π)*ρ_rain*N0_rain)))^((FT(3)+FT(b_rain)/FT(4)))
+    Ar_i = exp(FT(0.025)*(T-FT(273.16)))*FT(FT(π)/FT(4))*a_rain*N0_rain*Er_i*γ3br*(FT(ρ0/ρ))^(FT(0.5))*FT((ρ/(FT(π)*ρ_rain*N0_rain)))^(FT((FT(3)+b_rain)/FT(4)))
+    As_c = FT(FT(π)/FT(4))*a_snow*N0_snow*Es_c*γ3bs*(FT(ρ0/ρ))^(FT(0.5))*(FT(ρ/(FT(π)*ρ_snow*N0_snow)))^(FT((FT(3)+b_snow)/FT(4)))
+    As_i = exp(FT(0.025)*(T-FT(273.16)))*FT(FT(π)/FT(4))*a_snow*N0_snow*Es_i*γ3bs*(FT(ρ0/ρ))^(FT(0.5))*FT((ρ/(FT(π)*ρ_snow*N0_snow)))^(FT((FT(3)+b_snow)/FT(4)))
+    Ag_c = FT(FT(π)/FT(4))*a_graupel*N0_graupel*Eg_c*γ3bs*(FT(ρ0/ρ))^(FT(0.5))*FT((ρ/(FT(π)*ρ_graupel*N0_graupel)))^(FT((FT(3)+b_graupel)/FT(4)))
+    Ag_i = exp(FT(0.025)*(T-FT(273.16)))*FT(FT(π)/FT(4))*a_graupel*N0_graupel*Eg_c*γ3bs*(FT(ρ0/ρ))^(FT(0.5))*(FT(ρ/(FT(π)*ρ_graupel*N0_graupel)))^(FT((FT(3)+b_graupel)/FT(4)))
+
+    dqrdt = (Ar_c*qc + Ar_i*qi)*qr^(FT((FT(3)+b_rain)/FT(4)))
+    dqsdt = (As_c*qc + As_i*qi)*qs^(FT((FT(3)+b_snow)/FT(4)))
+    dqgdt = (Ag_c*qc + Ag_i*qi)*qg^(FT((FT(3)+b_graupel)/FT(4)))
+
+    ### Autoconversion
+
+    Auto = FT(max(FT(0.0), α*(qc - qc0)))
+
+    ### Aggregation
+
+    Aggr = FT(max(FT(0.0), β*exp(FT(0.025)*(T-FT(273.16)))*(qi-qi0)))
+
+    ### Evaporation
+
+    S = FT((qt-qn)/qsatt)
+    A_rain = FT((Lc/(Ka*T)))*FT(((Lc/(Rvap*T))) - FT(1))
+    A_snow_graupel = FT((Ls/(Ka*T)))*FT(((Ls/(Rvap*T))) - FT(1))
+    B_rain = FT(Rvap*Rair/(Da*e_satw))
+    B_snow_graupel = FT(Rvap*Rair/(Da*e_sati))
+    A_er = a_fr*FT((ρ/(FT(π)*ρ_rain*N0_rain)))^(FT(0.5))
+    A_es = a_fs*FT((ρ/(FT(π)*ρ_snow*N0_snow)))^(FT(0.5))
+    A_eg = a_fg*FT((ρ/(FT(π)*ρ_graupel*N0_graupel)))^(FT(0.5))
+    B_er = b_fr*FT((ρ*a_rain/μ))^(FT(0.5))*γ5br*FT((ρ0/ρ))^(FT(0.25))*FT((ρ/(FT(π)*ρ_rain*N0_rain)))^(FT((FT(5)+b_rain)/FT(8)))
+    B_es = b_fs*FT((ρ*a_snow/μ))^(FT(0.5))*γ5bs*FT((ρ0/ρ))^(FT(0.25))*FT((ρ/(FT(π)*ρ_snow*N0_snow)))^(FT((FT(5)+b_snow)/FT(8)))
+    B_eg = b_fg*FT((ρ*a_graupel/μ))^(FT(0.5))*γ5bg*FT((ρ0/ρ))^(FT(0.25))*FT((ρ/(FT(π)*ρ_graupel*N0_graupel)))^(FT((FT(5)+b_graupel)/FT(8)))
+
+    Evap_r = FT((FT(2)*FT(π)*C_rain*N0_rain)/(ρ*(A_rain+B_rain)))*(A_er*sqrt(qr) + B_er*qr^(FT((FT(5)+b_rain)/FT(8))))*(S-FT(1))
+    Evap_s = FT((FT(2)*FT(π)*C_snow*N0_snow)/(ρ*(A_snow_graupel+B_snow_graupel)))*(A_es*sqrt(qs) + B_es*qs^(FT((FT(5)+b_snow)/FT(8))))*(S-FT(1))
+    Evap_g = FT((FT(2)*FT(π)*C_graupel*N0_graupel)/(ρ*(A_snow_graupel+B_snow_graupel)))*(A_eg*sqrt(qg) + B_eg*qg^(FT((FT(5)+b_graupel)/FT(8))))*(S-FT(1))
+
+    ### cloud ice sedimentation is handled as part of the precipitation package, this routine ignores it
+
+    dqpdt = FT(clamp(dqrdt + dqsdt + dqgdt + Auto + Aggr + Evap_r + Evap_s + Evap_g,-qp,qn))
+
+    #if (qn > 0.001)
+        #@info dqrdt, dqsdt, dqgdt, Auto, Aggr, Evap_r, Evap_s, Evap_g, qn, qc, qi, qp, qr, qs, qg, dqpdt
+    #end
+
+    return FT(dqpdt)
+end
+
 function compute_Pm(ρ,qr, qs, qg, MicroConst)
     a_rain = MicroConst.a_rain #Constant in fall speed for rain
     γ4br = MicroConst.γ4br
@@ -366,6 +490,42 @@ function compute_Pm(ρ,qr, qs, qg, MicroConst)
     #    @info Pr, Ps, Pg
     #end
     return Pr, Ps, Pg
+end
+
+function compute_Pm_gpu(u, qe, qr, qs, qg, MicroConst, lpert)
+   
+    FT = eltype(u)
+    if (lpert)
+        ρ = u[1] + qe[1]
+    else
+        ρ = u[1]
+    end
+    a_rain = MicroConst.a_rain #Constant in fall speed for rain
+    γ4br = MicroConst.γ4br
+    ρ_rain = MicroConst.ρ_rain
+    N0_rain = MicroConst.N0_rain
+    b_rain = MicroConst.b_rain
+    ρ0 = MicroConst.ρ0
+
+    a_snow = MicroConst.a_snow #Constant in fall speed for rain
+    γ4bs = MicroConst.γ4bs
+    ρ_snow = MicroConst.ρ_snow
+    N0_snow = MicroConst.N0_snow
+    b_snow = MicroConst.b_snow
+
+    a_graupel = MicroConst.a_graupel #Constant in fall speed for rain
+    γ4bg = MicroConst.γ4bg
+    ρ_graupel = MicroConst.ρ_graupel
+    N0_graupel = MicroConst.N0_graupel
+    b_graupel = MicroConst.b_graupel
+
+    Pr = (a_rain * γ4br)/FT(6) * (FT(π) * ρ_rain * N0_rain)^(FT(-b_rain/FT(4)))*(ρ0/ρ)^(FT(0.5))*(ρ*qr)^(FT(1)+FT(b_rain/FT(4)))
+    Ps = (a_snow * γ4bs)/FT(6) * (FT(π) * ρ_snow * N0_snow)^(FT(-b_snow/FT(4)))*(ρ0/ρ)^(FT(0.5))*(ρ*qs)^(FT(1)+FT(b_snow/FT(4)))
+    Pg = (a_graupel * γ4bg)/FT(6) * (FT(π) * ρ_graupel * N0_graupel)^(FT(-b_graupel/FT(4)))*(ρ0/ρ)^(FT(0.5))*(ρ*qg)^(FT(1)+FT(b_graupel/FT(4)))
+    #if (Pr > 0)
+    #    @info Pr, Ps, Pg
+    #end
+    return FT(Pr), FT(Ps), FT(Pg)
 end
 
 function saturation_adjustment_sam_microphysics(ρ,hl,qt,qp,z,MicroConst,PhysConst)
@@ -511,6 +671,163 @@ function saturation_adjustment_sam_microphysics(ρ,hl,qt,qp,z,MicroConst,PhysCon
     P = moistPressure(PhysConst; ρ = ρ, Temp = T, qv = qt-qn)
     
     return T,P,qn,qc,qi,qr,qs,qg,qsatt
+end
+
+function saturation_adjustment_sam_microphysics_gpu(u,qe,z,MicroConst,PhysConst,lpert)
+
+    FT = eltype(u)
+    if (lpert)
+        ρ = u[1] + qe[1]
+        hl = (u[5] + qe[5])/ρ
+        qt = (u[6] + qe[6])/ρ
+        qp = (u[7] + qe[7])/ρ
+    else
+        ρ = u[1]
+        hl = u[5]/ρ
+        qt = u[6]/ρ
+        qp = u[7]/ρ
+    end
+
+    T00n = MicroConst.T00n #Temperature threshold for cloud water
+    T0n = MicroConst.T0n #Temperature threshold for ice
+    T00p = MicroConst.T00p #Temperature threshold for rain     
+    T0p = MicroConst.T0p #Temperature threshold for snow/graupel 
+    T00g = MicroConst.T00g #Temperature threshold for graupel
+    T0g = MicroConst.T0g #Temperature threshold for graupel
+    Lc = MicroConst.Lc
+    Lf = MicroConst.Lf
+    Ls = MicroConst.Ls
+    g=PhysConst.g
+    cp = PhysConst.cp
+    fac_cond = Lc/cp
+    fac_fus = Lf/cp
+    fac_sub = Ls/cp
+    # find equilibrium temperature from saturation adjustment
+    # initial guess for sensible temperature and pressure assumes no condensates/all vapor
+    T =  (hl - g*z)/cp
+    qt = FT(max(FT(0.0),qt))
+    qp = FT(max(FT(0.0),qp))
+    #P = moistPressure(PhysConst; ρ=ρ, Temp=T, qv = qt) 
+    an = FT(1/(T0n - T00n))
+    bn = T00n * an
+    ap = FT(1/(T0p - T00p))
+    bp = T00p*ap
+    fac1 = fac_cond+(FT(1)+bp)*fac_fus
+    fac2 = fac_fus*ap
+    ag = FT(FT(1)/(T0g - T00g))
+    T1 = T + fac1*qp/(FT(1)+fac2*qp)
+    P = moistPressure(PhysConst; ρ=ρ, Temp=T1, qv = qt)
+    #if (qp > 1e-8) 
+    #    @info qp, T, T1, fac1*qp/(1+fac2*qp), fac1*qp, (1+fac2*qp)
+    #end
+
+    if (T1 >= T0n)
+
+        T1 = T + fac_cond*qp
+        P = moistPressure(PhysConst; ρ=ρ, Temp=T1, qv = qt)
+        qsatt = FT(qsatw(T1, FT(P/FT(100))))
+
+    elseif (T1 <= T00n)
+
+        T1 = T + fac_sub*qp
+        P = moistPressure(PhysConst; ρ=ρ, Temp=T1, qv = qt)
+        qsatt = FT(qsati(T1, FT(P/FT(100))))
+
+    else
+
+        ωn = FT(max(FT(0),FT(min(FT(1),an*T1-bn))))
+        qsatt = ωn*FT(qsatw(T1,FT(P/FT(100))))+(1-ωn)*FT(qsati(T1,FT(P/FT(100))))
+
+    end
+
+    if (qt > qsatt)
+
+        niter = Int32(0)
+        dT = FT(100)
+        dqsat = FT(0.0)
+        while (abs(dT) > FT(0.001) && niter < FT(50))
+
+            if (T1 >= T0n)
+
+                ωn=1
+                lstarn = fac_cond
+                dlstarn = FT(0)
+                qsatt = FT(qsatw(T1,P/100))
+                dqsat = FT(dtqsatw(T1,P/100))
+
+            elseif (T1 <= T00n)
+
+                ωn = FT(0)
+                lstarn = fac_sub
+                dlstarn = FT(0)
+                qsatt = FT(qsati(T1,P/100))
+                dqsat = FT(dtqsati(T1,P/100))
+
+            else
+
+                ωn = FT(max(FT(0),FT(min(FT(1),an*T1-bn))))
+                lstarn = fac_cond+(FT(1)-ωn)*fac_fus
+                dlstarn = an*fac_fus
+                qsatt = ωn*FT(qsatw(T1,P/100)) + (FT(1)-ωn)*FT(qsati(T1,P/100))
+                dqsat = ωn*FT(dtqsati(T1,P/100)) + (FT(1)-ωn)*FT(dtqsati(T1,P/100))
+
+            end
+
+            if (T1 >= T0p)
+
+                ωp = FT(1)
+                lstarp = fac_cond
+                dlstarp = FT(0)
+
+            elseif (T1 <= T00p)
+
+                ωp = FT(0)
+                lstarp = fac_sub
+                dlstarp = FT(0)
+
+            else
+
+                ωp = FT(max(FT(0),FT(min(FT(1),ap*T1-bp))))
+                lstarp = fac_cond + (FT(1)-ωp)*fac_fus
+                dlstarp=ap*fac_fus
+
+            end
+
+            fff = T - T1 + lstarn*(qt - qsatt) + lstarp*qp
+            dfff = dlstarn*(qt - qsatt) - lstarn*dqsat - FT(1) + dlstarp*qp
+            dT = -fff/dfff
+            niter = niter + FT(1)
+            T1 = T1 + dT
+        end
+
+        qsatt = qsatt + dqsat * dT
+        qn  = max(FT(0.0), qt-qsatt)
+
+    else
+        qn = FT(0.0)
+    end
+
+    T = T1#= - fac1*qp/(1+fac2*qp)
+    if (T1 >= T0n)
+        T = T1 - fac_cond*qp
+    elseif (T1 <= T00n)
+        T = T1 - fac_sub*qp
+    end=#
+
+    qp = FT(max(FT(0.0), qp))
+    ωn = FT(max(FT(0),FT(min(FT(1),an*T-bn))))
+    ωp = FT(max(FT(0),FT(min(FT(1),ap*T-bp))))
+    ωg = FT(max(FT(0),FT(min(FT(1),ag*T-T00g*ag))))
+
+    qc = FT(max(FT(0.0),ωn*qn))
+    qi = FT(max(FT(0.0),(FT(1)-ωn)*qn))
+
+    qr = FT(max(FT(0.0),ωp*qp))
+    qs = FT(max(FT(0.0),(FT(1)-ωp)*(FT(1)-ωg)*qp))
+    qg = FT(max(FT(0.0),(FT(1)-ωp)*ωg*qp))
+    P = moistPressure(PhysConst; ρ = ρ, Temp = T, qv = qt-qn)
+
+    return FT(T),FT(P),FT(qn),FT(qc),FT(qi),FT(qr),FT(qs),FT(qg),FT(qsatt)
 end
 
 
