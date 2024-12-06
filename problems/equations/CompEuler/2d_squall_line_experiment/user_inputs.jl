0a8e1a4e2966db9985cc62630c41052151efd1b1
Simone <smarras@njit.edu>
2024-12-06 10:56:46 -0500

    WIP. added 2D squall. to be tested. missing kernel/physics/saturation.jl

--- problems/equations/CompEuler/2d_squall_line_experiment/user_inputs.jl
+++ problems/equations/CompEuler/2d_squall_line_experiment/user_inputs.jl
@@ -6,12 +6,12 @@ function user_inputs()
         :ode_solver           => SSPRK54(), #ORK256(),#SSPRK33(), #SSPRK33(), #SSPRK54(),
         :Δt                   => 0.2,
         :tinit                => 0.0,
-        :tend                 => 9000.0,
+        :tend                 => 100.0,
         #:tinit                => 100.0,
         #:tend                 => 1000.0,
         #:lrestart             => true,
         #:restart_input_file_path => "./output/CompEuler/theta/output-19Nov2023-115126",
-        :diagnostics_at_times => (100, 200, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 5000, 6000, 6500, 7000, 7500),
+        :diagnostics_at_times => [5, 10, 20, 30, 40, 50, 100], #, 200, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 5000, 6000, 6500, 7000, 7500),
         :case                 => "rtb",
         :lsource              => true, 
         :lmoist               => true,
@@ -33,7 +33,7 @@ function user_inputs()
         # Mesh paramters and files:
         #---------------------------------------------------------------------------
         :lread_gmsh          => true, #If false, a 1D problem will be enforced
-        :gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_squall_line.msh",
+        :gmsh_filename       => "./meshes/gmsh_grids/squall2d.msh",
         #---------------------------------------------------------------------------
         # Soundings and data files
         #---------------------------------------------------------------------------
