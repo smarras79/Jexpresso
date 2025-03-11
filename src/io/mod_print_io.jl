80658afb4aefbc6685cbe783d0bece0786005adc
Hang Wang <hangwang1229@gmail.com>
2025-03-11 10:40:15 -0400

    MPI periodic WIP

--- src/io/mod_print_io.jl
+++ src/io/mod_print_io.jl
@@ -1,10 +1,16 @@
-function println_rank(args...; msg_rank::Int = 0)
+function println_rank(args...; msg_rank::Int = 0, suppress = false)
+    if suppress == true
+        return
+    end
     if msg_rank == 0
         println(args...)
     end
 end
 
-function print_rank(args...; msg_rank::Int = 0)
+function print_rank(args...; msg_rank::Int = 0, suppress = false)
+    if suppress == true
+        return
+    end
     if msg_rank == 0
         print(args...)
     end
