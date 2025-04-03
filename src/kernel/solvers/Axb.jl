15353500b6d6e77e8a67e96b21ce155c88d9eb68
smarras79 <smarras@njit.edu>
2025-04-03 15:13:33 -0400

    push

--- src/kernel/solvers/Axb.jl
+++ src/kernel/solvers/Axb.jl
@@ -3,11 +3,12 @@ using LinearSolve: solve
 using SnoopCompile
 
 function solveAx(L, RHS, linear_solver...)
-    
-    #@info linear_solver
-    
+
+    @info " Solve Ax = b ................."
+
     prob = LinearProblem(L, RHS);    
     sol = solve(prob, linear_solver)
     
+    @info " Solve Ax = b ................. END"
     return sol
 end
