b2a6cef8a4c6e92f9b13a31c79edbe764eed0fa4
Simone <smarras@njit.edu>
2025-05-09 16:52:13 -0400

    setup HSmount_Lag for 1 m mountain.

--- problems/equations/CompEuler/tcf/user_primitives.jl
+++ problems/equations/CompEuler/tcf/user_primitives.jl
@@ -4,8 +4,6 @@ function user_primitives!(u,qe,uprimitive,::TOTAL)
     uprimitive[3] = u[3]/u[1]
     uprimitive[4] = u[4]/u[1]
     uprimitive[5] = u[5]/u[1]
-    uprimitive[6] = u[6]/u[1]
-    uprimitive[7] = u[7]/u[1]
 end
 
 function user_primitives!(u,qe,uprimitive,::PERT)
@@ -14,17 +12,6 @@ function user_primitives!(u,qe,uprimitive,::PERT)
     uprimitive[3] = u[3]/(u[1]+qe[1])
     uprimitive[4] = u[4]/(u[1]+qe[1])
     uprimitive[5] = (u[5]+qe[5])/(u[1]+qe[1])-qe[5]/qe[1]
-    uprimitive[6] = (u[6]+qe[6])/(u[1]+qe[1])-qe[6]/qe[1]
-    uprimitive[7] = (u[7]+qe[7])/(u[1]+qe[1])-qe[7]/qe[1]
-end
-
-function user_primitives_gpu(u,qe,lpert)
-    T = eltype(u)
-    if (lpert)
-        return T(u[1]+qe[1]), T(u[2]/(u[1]+qe[1])), T(u[3]/(u[1]+qe[1])), T(u[4]/(u[1]+qe[1])), T((u[5]+qe[5])/(u[1]+qe[1]) - qe[5]/qe[1]), T((u[6]+qe[6])/(u[1]+qe[1])-qe[6]/qe[1]), T((u[7]+qe[7])/(u[1]+qe[1])-qe[7]/qe[1])
-    else
-        return T(u[1]), T(u[2]/u[1]), T(u[3]/u[1]), T(u[4]/u[1]), T(u[5]/u[1]), T(u[6]/u[1]), T(u[7]/u[1])
-    end
 end
 
 function user_uout!(uout, u, qe, ::TOTAL)
@@ -34,7 +21,5 @@ function user_uout!(uout, u, qe, ::TOTAL)
     uout[3] = u[3]/u[1]
     uout[4] = u[4]/u[1]
     uout[5] = u[5]/u[1]
-    uout[6] = u[6]/u[1]
-    uout[7] = u[7]/u[1]
 
 end
