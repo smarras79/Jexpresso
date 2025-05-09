e157270feab404ac395f120e7202adf7df89b3bd
Simone <smarras@njit.edu>
2025-05-09 16:52:13 -0400

    setup HSmount_Lag for 1 m mountain.

--- problems/equations/CompEuler/HSmount_Lag/user_inputs.jl
+++ problems/equations/CompEuler/HSmount_Lag/user_inputs.jl
@@ -3,11 +3,11 @@ function user_inputs()
         #---------------------------------------------------------------------------
         # User define your inputs below: the order doesn't matter
         #---------------------------------------------------------------------------
-        :tend                 => 500,#000.0, #2π,
+        :tend                 => 2000.0, #2π,
         :Δt                   => 0.1,#8.75e-4,
         :ode_solver           => SSPRK54(),
         #:ndiagnostics_outputs => 10,
-        :diagnostics_at_times => range(0,500,30),
+        :diagnostics_at_times => range(0,2000,30),
         :output_dir           => "./output/",
         :case                 => "rtb",
         #:CL                   => NCL(),
@@ -52,10 +52,10 @@ function user_inputs()
         #---------------------------------------------------------------------------
         # Mountain parameters
         #---------------------------------------------------------------------------
-        :lwarp               => false,
+        :lwarp               => true,
         :mount_type          => "agnesi",
         :a_mount             => 10000.0,
-        :h_mount             => 0.0, #1.0
+        :h_mount             => 1.0,
         :c_mount             => 0.0,
         #---------------------------------------------------------------------------
         # Filter parameters
