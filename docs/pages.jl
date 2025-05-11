d8c849584c72dc03db3b851b0cc48ebd355d3777
Simone <smarras@njit.edu>
2025-05-11 12:34:35 -0400

    wip docs

--- docs/pages.jl
+++ docs/pages.jl
@@ -2,6 +2,5 @@ pages = ["index.md",
          "Performance" => "features/performance.md",
          "Best practices" => "features/best-practices.md",
          "User inputs" => "tutorials/user_inputs.md",
-         "Tutorials" => "tutorials/running-jexpresso.md",
-         "Tutorials" => "tutorials/newcase.md"
+         "Tutorials" => "tutorials/running-jexpresso.md", "tutorials/newcase.md"
          ]
