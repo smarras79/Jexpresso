b164128e32f7bbd1b5bd6bb9ec5955d15e14ab93
smarras79 <smarras@njit.edu>
2025-06-09 08:26:28 -0400

    added LES for Intercomp. Project

--- problems/equations/CompEuler/LES/user_inputs.jl
+++ problems/equations/CompEuler/LES/user_inputs.jl
@@ -1,34 +1,39 @@
 function user_inputs()
-    
     inputs = Dict(
         #---------------------------------------------------------------------------
         # User define your inputs below: the order doesn't matter
         #---------------------------------------------------------------------------
         :ode_solver           => SSPRK54(), #ORK256(),#SSPRK33(), #SSPRK33(), #SSPRK54(),
-        #:Δt                   => 0.02,
-        :Δt                   => 0.4,
+        :Δt                   => 2.0,
         :tinit                => 0.0,
-        :tend                 => 1000.0,
-        :diagnostics_at_times => (0:100:1000),
-        :case                 => "rtb",
-        :lsource              => true, 
-        #:SOL_VARS_TYPE        => PERT(), #TOTAL() is default
+        :tend                 => 10000,
+        #:tinit                => 100.0,
+        #:tend                 => 1000.0,
+        #:lrestart             => true,
+        #:restart_input_file_path => "",
+        :diagnostics_at_times => (0:500:10000),
+        #:case                 => "",
+        :lsource              => true,
         #---------------------------------------------------------------------------
         #Integration and quadrature properties
         #---------------------------------------------------------------------------
-        :interpolation_nodes =>"lgl",
-        :nop                 => 4,      # Polynomial order
+        :interpolation_nodes  =>"lgl",
+        :nop                  => 3,      # Polynomial order
         #---------------------------------------------------------------------------
         # Physical parameters/constants:
         #---------------------------------------------------------------------------
         :lvisc                => true, #false by default NOTICE: works only for Inexact
-        :μ                   => [0.0, 125.0, 125.0, 125.0], #horizontal viscosity constant for momentum
+        #:visc_model           => SMAG(),
+        :visc_model           => AV(),
+        :ivisc_equations      => [1, 2, 3, 4, 5],
+        # smagorinsky, cs = 0.23, input cs^2 for momentum cs^2/Pr for other equations, where Pr = 1/3
+        :μ                    => [0.0, 0.53, 0.53, 0.53, 1.6], #horizontal viscosity constant for momentum
         #---------------------------------------------------------------------------
         # Mesh paramters and files:
         #---------------------------------------------------------------------------
-        :lread_gmsh          => true, #If false, a 1D problem will be enforced
-        :gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_10x10_periodic.msh", #for nop=4
-        #:gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_10x10.msh", #for nop=4
+        :lread_gmsh           => true, #If false, a 1D problem will be enforced
+        :gmsh_filename       => "./meshes/gmsh_grids/LESICP_10240x10240x5000.msh",
+        #:gmsh_filename       => "./meshes/gmsh_grids/LESICP.msh",
         #---------------------------------------------------------------------------
         # Filter parameters
         #---------------------------------------------------------------------------
@@ -40,25 +45,9 @@ function user_inputs()
         # Plotting parameters
         #---------------------------------------------------------------------------
         :outformat           => "vtk",
-        :loverwrite_output   => true,
+        :output_dir          => "./output/",
+        :loverwrite_output   => true,  #this is only implemented for VTK for now
         :lwrite_initial      => true,
-        :output_dir          => "./output",
-        #:output_dir          => "./test/CI-run",
-        :loutput_pert        => true,  #this is only implemented for VTK for now
-        #---------------------------------------------------------------------------
-        # init_refinement
-        #---------------------------------------------------------------------------
-        :linitial_refine     => false,
-        :init_refine_lvl     => 1,
-        #---------------------------------------------------------------------------
-        # AMR
-        #---------------------------------------------------------------------------
-        :ladapt              => false,
-        #---------------------------------------------------------------------------
-        # AMR parameters
-        #---------------------------------------------------------------------------
-        :amr_freq            => 200,
-        :amr_max_level       => 2,
         #---------------------------------------------------------------------------
     ) #Dict
     #---------------------------------------------------------------------------
