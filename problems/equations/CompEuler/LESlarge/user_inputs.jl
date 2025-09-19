f5180963e55e923e59a8f99fd4fc0d8e3bd0d62b
smarras79 <smarras@njit.edu>
2025-09-19 08:45:46 +0200

    added LESlarge with large domain closer to real LEICP. Still converging to the full domain.

--- problems/equations/CompEuler/LESlarge/user_inputs.jl
+++ problems/equations/CompEuler/LESlarge/user_inputs.jl
@@ -4,7 +4,7 @@ function user_inputs()
         # User define your inputs below: the order doesn't matter
         #---------------------------------------------------------------------------
         :ode_solver           => SSPRK54(), #ORK256(),#SSPRK33(), #SSPRK33(), #SSPRK54(),
-        :Δt                   => 0.1,
+        :Δt                   => 0.025,
         :tinit                => 0.0,
         :tend                 => 7200,
         #:tinit                => 100.0,
@@ -36,9 +36,9 @@ function user_inputs()
         #---------------------------------------------------------------------------
         :lread_gmsh          => true, #If false, a 1D problem will be enforced
         #:gmsh_filename       => "./meshes/gmsh_grids/LESICP_10240x10240x5000.msh",
+        :gmsh_filename       => "./meshes/gmsh_grids/LESICP_64x64x32.msh",
         #:gmsh_filename       => "./meshes/gmsh_grids/LESICP_16x1x16.msh",
         #:gmsh_filename       => "./meshes/gmsh_grids/LESICP_32x32x16.msh",
-        :gmsh_filename       => "./meshes/gmsh_grids/LESICP_64x32x32.msh",
         #:linitial_refine     => false,
         :init_refine_lvl     => 1,
         #---------------------------------------------------------------------------
