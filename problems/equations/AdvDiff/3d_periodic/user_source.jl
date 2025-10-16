206e39aceed23fc74ef0cccd2183797bfd0269e3
Hang Wang <hangwang1229@gmail.com>
2025-10-16 10:58:06 -0400

    change aseemble_mpi_v3! to assemble_mpi!

--- problems/equations/AdvDiff/3d_periodic/user_source.jl
+++ problems/equations/AdvDiff/3d_periodic/user_source.jl
@@ -1,10 +1,16 @@
 function user_source!(S,
-                      q, 
-                      qe,
-                      npoin::Int64,
-                      ::CL, ::TOTAL;
-                      neqs=1)
-    
+                    q, 
+                    qe,
+                    npoin::Int64,
+                    ::CL, ::TOTAL;
+                    neqs=1,
+                    x=0.0,
+                    y=0.0,
+                    z=0.0,
+                    xmin=0.0,xmax=0.0,
+                    ymin=0.0,ymax=0.0,
+                    zmin=0.0,zmax=0.0)
+
     PhysConst = PhysicalConst{Float64}()
         
     #
@@ -20,7 +26,13 @@ function user_source!(S,
                       qe,
                       npoin::Int64,
                       ::CL, ::PERT;
-                      neqs=1)
+                      neqs=1,
+                      x=0.0,
+                      y=0.0,
+                      z=0.0,
+                      xmin=0.0, xmax=0.0,
+                      ymin=0.0, ymax=0.0,
+                      zmin=0.0, zmax=0.0)
 
     PhysConst = PhysicalConst{Float64}()
 
