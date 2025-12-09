7dcb547058cf0fe082bf86bab3f625584157b953
smarras79 <smarras@njit.edu>
2025-12-09 12:21:50 +0100

    Fixed user_source.jl for elliptic problems

--- problems/equations/Elliptic/2dlaplace/user_source.jl
+++ problems/equations/Elliptic/2dlaplace/user_source.jl
@@ -1,10 +1,14 @@
-function user_source(S,
+function user_source!(S,
                       q, 
                       qe,
                       npoin::Int64,
                       ::CL, ::TOTAL;
-                      neqs=1,x=0.0, y=0.0, ymin=0.0, ymax=30000.0, xmin = -120000, xmax =120000)
-
+                      neqs=1,
+                      x=1.0, y=1.0,
+                      xmax=1.0, xmin=0.0,
+                      ymax=1.0, ymin=0.0)
+  
+    
     PhysConst = PhysicalConst{Float64}()
     
     #
