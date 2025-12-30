14f647482fb05605bcadab25c5e1a9bd909a1314
smarras79 <smarras@njit.edu>
2025-12-30 15:10:50 +0100

    fixed user_source! definitions on Helmhotz case1 and case1_Laguerre

--- problems/equations/Helmholtz/case1/user_source.jl
+++ problems/equations/Helmholtz/case1/user_source.jl
@@ -3,7 +3,10 @@ function user_source!(S,
                       qe,
                       npoin::Int64,
                       ::CL, ::TOTAL;
-                      neqs=1,x=0.0, y=0.0)
+                      neqs=1,
+                      x=0.0, y=0.0,
+                      xmax=1.0, xmin=0.0,
+                      ymax=1.0, ymin=0.0)
 
     PhysConst = PhysicalConst{Float64}()
     
