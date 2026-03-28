5b654b2b8a7815c40647c8fb41b50c28da18a739
Hang Wang <hangwang1229@gmail.com>
2026-03-28 12:43:50 -0400

    if no new refinment occurred, stop refining

--- src/kernel/mesh/mesh.jl
+++ src/kernel/mesh/mesh.jl
@@ -4657,8 +4657,8 @@ function mod_mesh_mesh_driver(inputs::Dict, nparts, distribute, args...)
                 p_model_r         = partitioned_model_tmp
                 mesh_r_o          = mesh_tmp
                 p_model_r_o       = partitioned_model_tmp
-                cnt               = 0
-                while current_max_ad_lv < max_ad_lv && cout < 4
+                while current_max_ad_lv < max_ad_lv
+                    prev_max_ad_lv = current_max_ad_lv
                     mesh_r = St_mesh{TInt,TFloat, CPU()}(nsd=TInt(inputs[:nsd]),
                                             nop=TInt(inputs[:nop]),
                                             ngr=TInt(inputs[:nop_laguerre]+1),
@@ -4667,9 +4667,12 @@ function mod_mesh_mesh_driver(inputs::Dict, nparts, distribute, args...)
                     do_preadapt!(ref_coarse_flags, inputs, mesh_r_o)
                     p_model_r, n2o_ele_map_tmp = mod_mesh_read_gmsh!(mesh_r, inputs, nparts, distribute, ref_coarse_flags, p_model_r_o, mesh_r_o)
                     current_max_ad_lv = MPI.Allreduce(maximum(mesh_r.ad_lvl), MPI.MAX, comm)
+                    if current_max_ad_lv == prev_max_ad_lv
+                        println(" No new refinement occurred: stop refining")
+                        break
+                    end
                     mesh_r_o          = mesh_r
                     p_model_r_o       = p_model_r
-                    cnt              += 1
                 end
                 mesh              = mesh_r
                 partitioned_model = p_model_r
