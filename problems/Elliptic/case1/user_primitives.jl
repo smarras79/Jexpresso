6a59760e8555593a16e9adf762884b2d138220f7
smarras79 <smarras@njit.edu>
2026-06-10 09:29:20 +0200

    Fixed minor in Elliptic/case1 and Elliptic/2dlaplace

--- problems/Elliptic/case1/user_primitives.jl
+++ problems/Elliptic/case1/user_primitives.jl
@@ -1,3 +1,7 @@
-function user_uout!(ip, ET, uout, u, qe; kwargs...)
+function user_uout!(ip, ::TOTAL, uout, u, qe; kwargs...)
     uout[1] = u[1]
+end
+
+function user_uout!(ip, ::PERT, uout, u, qe; kwargs...)
+    uout[1] = u[1] + qe[1]
 end
