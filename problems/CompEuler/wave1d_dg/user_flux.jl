f26c7e7fcab637caa4ae602773eba829b8c5be22
Antone <ajchacartegui@gmail.com>
2026-07-23 09:11:47 -0600

    DG 1D: wave1d_dg case (first end-to-end DG smoke)
    New 1D DG case: wave1d with :AD => DiscGal(), :numerical_flux => upwind_flux(),
    periodic. user_flux.jl carries the max-wave-speed hook user_max_wave_speed
    (lambda_max = 1 for the acoustic system). First conforming fixed-mesh 1D DG
    run; smoke green.

---
+++ problems/CompEuler/wave1d_dg/user_flux.jl
@@ -0,0 +1,22 @@
+function user_flux!(F, G, SD::NSD_1D,
+                    q,
+                    qe,
+                    mesh::St_mesh,
+                    ::CL, ::TOTAL; neqs=4, ip=1)
+
+    F[1] = q[2]
+    F[2] = q[1]
+    
+end
+
+function user_flux_gpu(q,qe,PhysConst,lpert)
+    T = eltype(q)
+    return T(q[2]), T(q[1])
+end
+
+# Max wave-speed hook for the DG numerical flux (Q3).
+# wave1d is the linear acoustic system u_t + A u_x = 0, A = [0 1; 1 0],
+# eigenvalues ±1 ⇒ λ_max = 1.
+function user_max_wave_speed(q, qe, SD::NSD_1D, ::TOTAL; neqs=2)
+    return 1.0
+end
