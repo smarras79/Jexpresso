fc4b357b45052251ca8b862b22772a9cb5718e6c
Simone <smarras@njit.edu>
2026-08-08 11:55:52 -0400

    Added CI-ref/theta

--- test/CI-ref/CompEuler/theta/output/user_inputs.jl
+++ test/CI-ref/CompEuler/theta/output/user_inputs.jl
@@ -4,21 +4,18 @@ function user_inputs()
         #---------------------------------------------------------------------------
         # User define your inputs below: the order doesn't matter
         #---------------------------------------------------------------------------
-        :ode_solver           => SSPRK54(), #ORK256(),#SSPRK33(), #SSPRK33(), #SSPRK54(),
-        #:Δt                   => 0.02,
-        :Δt                   => 0.4,
+        :ode_solver           => CarpenterKennedy2N54(), #SSPRK54(), #ORK256(),#SSPRK33(), #SSPRK33(),
+        :Δt                   => 0.5,
         :tinit                => 0.0,
         :tend                 => 1000.0,
-        #:tinit                => 100.0,
-        #:tend                 => 1000.0,
-        #:lrestart             => true,
-        :restart_input_file_path => "./output/CompEuler/theta/output-19Nov2023-115126",
-        :ndiagnostics_outputs => 2,
-        #:diagnostics_at_times => (10, 50, 100, 200, 300),
+        :diagnostics_at_times => (1000),
+        :restart_time         => 50000,
+        :lrestart             => false,
+        #:CL                   => NCL(),
+        :restart_input_file_path => "/home/leon/njit/Jexpresso_gigales/Jexpresso/problems/equations/CompEuler/theta",
         :case                 => "rtb",
         :lsource              => true, 
-        #:backend              => MetalBackend(),
-        #:SOL_VARS_TYPE        => PERT(), #TOTAL() is default
+        :SOL_VARS_TYPE        => PERT(), #TOTAL() is default
         #---------------------------------------------------------------------------
         #Integration and quadrature properties
         #---------------------------------------------------------------------------
@@ -27,16 +24,18 @@ function user_inputs()
         #---------------------------------------------------------------------------
         # Physical parameters/constants:
         #---------------------------------------------------------------------------
-        :lvisc                => true, #false by default NOTICE: works only for Inexact
-        :ivisc_equations      => [1, 2, 3, 4],
-        :μ                   => [0.0, 20.0, 20.0, 60.0], #horizontal viscosity constant for momentum
+        :lvisc          => true, #false by default NOTICE: works only for Inexact       
+        :visc_model     => AV(),
+        #:visc_model     => VREM(),
+        #:visc_model     => SMAG(),
+        :energy_equation => "theta",
+        #:μ              => [0.0, 1.0, 1.0, 2.0], #horizontal viscosity constant for momentum
+        :μ              => [0.0, 125.0, 125.0, 125.0], #horizontal viscosity constant for momentum
         #---------------------------------------------------------------------------
         # Mesh paramters and files:
         #---------------------------------------------------------------------------
         :lread_gmsh          => true, #If false, a 1D problem will be enforced
-        #:gmsh_filename       => "./meshes/gmsh_grids/square_UNSTR_20el.msh", #for nop=4
-        #:gmsh_filename       => "./meshes/gmsh_grids/plate_hole.msh", #for nop=4
-        :gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_RTB20x20.msh", #for nop=4
+        :gmsh_filename       => "./meshes/gmsh_grids/hexa_TFI_10x10.msh", #for nop=4
         #---------------------------------------------------------------------------
         # Filter parameters
         #---------------------------------------------------------------------------
@@ -47,12 +46,26 @@ function user_inputs()
         #---------------------------------------------------------------------------
         # Plotting parameters
         #---------------------------------------------------------------------------
-        :outformat           => "hdf5",
+        :outformat           => "vtk",
         :loverwrite_output   => true,
-        :lwrite_initial      => false,
+        :lwrite_initial      => true,
         #:output_dir          => "./output",
-        :output_dir          => "./test/CI-ref",
+        :output_dir          => "./test/CI-run",
         :loutput_pert        => true,  #this is only implemented for VTK for now
+        #---------------------------------------------------------------------------
+        # init_refinement
+        #---------------------------------------------------------------------------
+        :linitial_refine     => false,
+        :init_refine_lvl     => 1,
+        #---------------------------------------------------------------------------
+        # AMR
+        #---------------------------------------------------------------------------
+        :ladapt              => false,
+        #---------------------------------------------------------------------------
+        # AMR parameters
+        #---------------------------------------------------------------------------
+        :amr_freq            => 200,
+        :amr_max_level       => 2,
         #---------------------------------------------------------------------------
     ) #Dict
     #---------------------------------------------------------------------------
