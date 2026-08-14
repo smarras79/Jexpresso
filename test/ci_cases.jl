89c7e7f6be97108631446084805858e8b990b909
Simone <smarras@njit.edu>
2026-08-14 08:58:57 -0400

    pull other CI tests

--- test/ci_cases.jl
+++ test/ci_cases.jl
@@ -74,6 +74,7 @@ const CI_CASES = CICase[
     CICase(eqs = "CompEuler", case = "sod1d", timeout = 10),
     CICase(eqs = "ShallowWater", case = "SoliWaveIsland", timeout = 11),
     CICase(eqs = "AdvDiff", case = "Wave_Train", timeout = 10),
+    CICase(eqs = "CompEuler", case = "thetaTracers", timeout = 20),
     #<< test/generate_ci_ref.jl inserts new cases above this line >>
 
     #--------------------------------------------------------------------------
