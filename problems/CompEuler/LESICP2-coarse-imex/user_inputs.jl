f6c760669ec243d5f63f8cd167e44bd2664582e0
Simone <smarras@njit.edu>
2026-08-23 19:25:17 -0400

    Added isotropic grid 60x2x60

--- problems/CompEuler/LESICP2-coarse-imex/user_inputs.jl
+++ problems/CompEuler/LESICP2-coarse-imex/user_inputs.jl
@@ -400,7 +400,8 @@ function user_inputs()
         # Byte-identical to LESICP2-coarse-hevi's, deliberately: this case exists
         # to be compared against that one, and a comparison across two meshes is
         # not a comparison.
-        :gmsh_filename    => "./problems/CompEuler/LESICP2-coarse-imex/LESICP_8x2x60_6400mX1600mX5000m.msh",
+        #:gmsh_filename    => "./problems/CompEuler/LESICP2-coarse-imex/LESICP_8x2x60_6400mX1600mX5000m.msh",
+        :gmsh_filename    => "./problems/CompEuler/LESICP2-coarse-imex/LESICP_60x2x60_6400mX1600mX5000m.msh",
         #:gmsh_filename    => "./meshes/gmsh_grids/LESICP_64x64x60_10kmX10kmX5km.msh",
 		
         # Warping:
