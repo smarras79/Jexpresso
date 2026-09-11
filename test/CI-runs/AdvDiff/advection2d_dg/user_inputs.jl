321aac23bab06f40863ccb526a749b121cf6a108
Antone <ajchacartegui@gmail.com>
2026-09-11 15:40:08 -0600

    Add AdvDiff/advection2d_dg to CI
    Registers the 2D DG advection case as a CI test. The 10x20 periodic
    mesh (21 KB) moves next to the deck so the case is self-contained in
    the repository, as CI requires; the deck points at the new location.
    160 steps on 5000 degrees of freedom, about 40 seconds, exercising the
    2D DG numbering, the face list with periodic pairing, the surface term
    and the DiscGal dispatch.

--- test/CI-runs/AdvDiff/advection2d_dg/user_inputs.jl
+++ test/CI-runs/AdvDiff/advection2d_dg/user_inputs.jl
@@ -32,7 +32,7 @@ function user_inputs()
         # Mesh paramters and files:
         #---------------------------------------------------------------------------
         :lread_gmsh          => true, #If false, a 1D problem will be enforced
-        :gmsh_filename       => "./problems/AdvDiff/advection2d_dg/hexa_TFI_10x20_periodic.msh", #for nop=4
+        :gmsh_filename       => "./test/CI-runs/AdvDiff/advection2d_dg/hexa_TFI_10x20_periodic.msh", #for nop=4
         #---------------------------------------------------------------------------
         # Plotting parameters
         #---------------------------------------------------------------------------
